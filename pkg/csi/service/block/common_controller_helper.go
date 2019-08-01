0be6b164d675f2cd4de7cdb15f12a93b5f4d6677
bdontu <bdontu@vmware.com>
2019-07-31 17:22:19 -0700

    Implement WCP CSI Attach by integrating with Pod Listener gRPC service + Implement WCP CSI Detach

--- pkg/csi/service/block/common_controller_helper.go
+++ pkg/csi/service/block/common_controller_helper.go
@@ -17,11 +17,41 @@ limitations under the License.
 package block
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"k8s.io/klog"
 )
+
+// ValidateCreateVolumeRequest is the helper function to validate
+// CreateVolumeRequest for all block controllers.
+// Function returns error if validation fails otherwise returns nil.
+func ValidateCreateVolumeRequest(req *csi.CreateVolumeRequest) error {
+	// Volume Name
+	volName := req.GetName()
+	if len(volName) == 0 {
+		msg := "Volume name is a required parameter."
+		klog.Error(msg)
+		return status.Error(codes.InvalidArgument, msg)
+	}
+	// Validate Volume Capabilities
+	volCaps := req.GetVolumeCapabilities()
+	if len(volCaps) == 0 {
+		return status.Error(codes.InvalidArgument, "Volume capabilities not provided")
+	}
+	if !IsValidVolumeCapabilities(volCaps) {
+		return status.Error(codes.InvalidArgument, "Volume capabilities not supported")
+	}
+	// Validate volume size exists in spec
+	if req.GetCapacityRange() == nil || req.GetCapacityRange().RequiredBytes == 0 {
+		return status.Error(codes.InvalidArgument, "Volume size is a required parameter")
+	}
+	return nil
+}
 
 // ValidateDeleteVolumeRequest is the helper function to validate
 // DeleteVolumeRequest for all block controllers.
@@ -32,6 +62,78 @@ func ValidateDeleteVolumeRequest(req *csi.DeleteVolumeRequest) error {
 		msg := "Volume ID is a required parameter."
 		klog.Error(msg)
 		return status.Errorf(codes.InvalidArgument, msg)
+	}
+	return nil
+}
+
+// ValidateControllerPublishVolumeRequest is the helper function to validate
+// ControllerPublishVolumeRequest for all block controllers.
+// Function returns error if validation fails otherwise returns nil.
+func ValidateControllerPublishVolumeRequest(req *csi.ControllerPublishVolumeRequest) error {
+	//check for required parameters
+	if len(req.VolumeId) == 0 {
+		msg := "Volume ID is a required parameter."
+		klog.Error(msg)
+		return status.Error(codes.InvalidArgument, msg)
+	} else if len(req.NodeId) == 0 {
+		msg := "Node ID is a required parameter."
+		klog.Error(msg)
+		return status.Error(codes.InvalidArgument, msg)
+	}
+	volCap := req.GetVolumeCapability()
+	if volCap == nil {
+		return status.Error(codes.InvalidArgument, "Volume capability not provided")
+	}
+	caps := []*csi.VolumeCapability{volCap}
+	if !IsValidVolumeCapabilities(caps) {
+		return status.Error(codes.InvalidArgument, "Volume capability not supported")
+	}
+	return nil
+}
+
+// ValidateControllerUnpublishVolumeRequest is the helper function to validate
+// ControllerUnpublishVolumeRequest for all block controllers.
+// Function returns error if validation fails otherwise returns nil.
+func ValidateControllerUnpublishVolumeRequest(req *csi.ControllerUnpublishVolumeRequest) error {
+	//check for required parameters
+	if len(req.VolumeId) == 0 {
+		msg := "Volume ID is a required parameter."
+		klog.Error(msg)
+		return status.Error(codes.InvalidArgument, msg)
+	} else if len(req.NodeId) == 0 {
+		msg := "Node ID is a required parameter."
+		klog.Error(msg)
+		return status.Error(codes.InvalidArgument, msg)
+	}
+	return nil
+}
+
+// CheckAPI checks if specified version is 6.7.3 or higher
+func CheckAPI(version string) error {
+	items := strings.Split(version, ".")
+	if len(items) < 2 || len(items) > 3 {
+		return fmt.Errorf("Invalid API Version format")
+	}
+	major, err := strconv.Atoi(items[0])
+	if err != nil {
+		return fmt.Errorf("Invalid Major Version value")
+	}
+	minor, err := strconv.Atoi(items[1])
+	if err != nil {
+		return fmt.Errorf("Invalid Minor Version value")
+	}
+
+	if major < MinSupportedVCenterMajor || (major == MinSupportedVCenterMajor && minor < MinSupportedVCenterMinor) {
+		return fmt.Errorf("The minimum supported vCenter is 6.7.3")
+	}
+
+	if major == MinSupportedVCenterMajor && minor == MinSupportedVCenterMinor {
+		if len(items) == 3 {
+			patch, err := strconv.Atoi(items[2])
+			if err != nil || patch < MinSupportedVCenterPatch {
+				return fmt.Errorf("Invalid patch version value")
+			}
+		}
 	}
 	return nil
 }
--- pkg/csi/service/block/wcp/controller_helper.go
+++ pkg/csi/service/block/wcp/controller_helper.go
@@ -17,89 +17,183 @@ limitations under the License.
 package wcp
 
 import (
+	"context"
+	"errors"
+	"flag"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/vim25/types"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"k8s.io/klog"
+	"sigs.k8s.io/vsphere-csi-driver/pkg/common/cns-lib/vsphere"
+	"sigs.k8s.io/vsphere-csi-driver/pkg/common/config"
 	"sigs.k8s.io/vsphere-csi-driver/pkg/csi/service/block"
+	"sigs.k8s.io/vsphere-csi-driver/pkg/syncer/podlistener"
 )
 
 const (
-	// MinSupportedVCenterMajor is the minimum, major version of vCenter
-	// on which WCP CSI controller is supported.
-	MinSupportedVCenterMajor int = 6
-
-	// MinSupportedVCenterMinor is the minimum, minor version of vCenter
-	// on which WCP CSI controller is supported.
-	MinSupportedVCenterMinor int = 7
+	defaultPodListenerServicePort = 10000
 )
 
-// checkAPI checks if specified version is 6.7.1 or higher
-func checkAPI(version string) error {
-	items := strings.Split(version, ".")
-	if len(items) <= 1 || len(items) > 3 {
-		return fmt.Errorf("Invalid API Version format")
-	}
-	major, err := strconv.Atoi(items[0])
-	if err != nil {
-		return fmt.Errorf("Invalid Major Version value")
-	}
-	minor, err := strconv.Atoi(items[1])
-	if err != nil {
-		return fmt.Errorf("Invalid Minor Version value")
-	}
-
-	if major < MinSupportedVCenterMajor || (major == MinSupportedVCenterMajor && minor < MinSupportedVCenterMinor) {
-		return fmt.Errorf("The minimum supported vCenter is 6.7.1")
-	}
-
-	if major == MinSupportedVCenterMajor && minor == MinSupportedVCenterMinor && len(items) == 2 {
-		patch, err := strconv.Atoi(items[2])
-		if err != nil || patch < 1 {
-			return fmt.Errorf("Invalid patch version value")
-		}
-	}
-	return nil
-}
-
-// validateCreateVolumeRequest is the helper function to validate
-// CreateVolumeRequest. Function returns error if validation fails otherwise returns nil.
-func validateCreateVolumeRequest(req *csi.CreateVolumeRequest) error {
+// ValidateCreateVolumeRequest is the helper function to validate
+// CreateVolumeRequest for WCP CSI driver.
+// Function returns error if validation fails otherwise returns nil.
+func validateWCPCreateVolumeRequest(req *csi.CreateVolumeRequest) error {
 	// Get create params
 	params := req.GetParameters()
-	// Validate volume parameters
-	if params != nil {
-		for paramName := range params {
-			if paramName != block.AttributeStoragePolicyID {
-				msg := fmt.Sprintf("volume parameter %s is not a valid parameter.", paramName)
-				return status.Error(codes.InvalidArgument, msg)
+	for paramName := range params {
+		paramName = strings.ToLower(paramName)
+		if paramName != block.AttributeStoragePolicyID && paramName != block.AttributeFsType {
+			msg := fmt.Sprintf("Volume parameter %s is not a valid WCP CSI parameter.", paramName)
+			return status.Error(codes.InvalidArgument, msg)
+		}
+	}
+	return block.ValidateCreateVolumeRequest(req)
+}
+
+// validateWCPDeleteVolumeRequest is the helper function to validate
+// DeleteVolumeRequest for WCP CSI driver.
+// Function returns error if validation fails otherwise returns nil.
+func validateWCPDeleteVolumeRequest(req *csi.DeleteVolumeRequest) error {
+	return block.ValidateDeleteVolumeRequest(req)
+}
+
+// validateWCPControllerPublishVolumeRequest is the helper function to validate
+// ControllerPublishVolumeRequest for WCP CSI driver. Function returns error if validation fails otherwise returns nil.
+func validateWCPControllerPublishVolumeRequest(req *csi.ControllerPublishVolumeRequest) error {
+	return block.ValidateControllerPublishVolumeRequest(req)
+}
+
+// validateWCPControllerUnpublishVolumeRequest is the helper function to validate
+// ControllerUnpublishVolumeRequest for WCP CSI driver. Function returns error if validation fails otherwise returns nil.
+func validateWCPControllerUnpublishVolumeRequest(req *csi.ControllerUnpublishVolumeRequest) error {
+	return block.ValidateControllerUnpublishVolumeRequest(req)
+}
+
+// getVMUUIDFromPodListenerService gets the vmuuid from pod listener gRPC service
+func getVMUUIDFromPodListenerService(volumeID string, nodeName string) (string, error) {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+	port := getPodListenerServicePort()
+	podListenerServiceAddr := flag.String("pod_listener_service", "127.0.0.1:"+strconv.Itoa(port), "The server address in the format of host:port")
+	// Connect to pod listerner gRPC service
+	conn, err := grpc.Dial(*podListenerServiceAddr, opts...)
+	if err != nil {
+		klog.Errorf("Failed to establish the connection to pod listener service when processing attach for volumeID: %s. Error: %+v", volumeID, err)
+		return "", err
+	}
+	defer conn.Close()
+
+	// Create a client stub for pod Listener gRPC service
+	client := podlistener.NewPodListenerClient(conn)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Call GetPodVMUUIDAnnotation method on the client stub
+	res, err := client.GetPodVMUUIDAnnotation(ctx,
+		&podlistener.PodListenerRequest{
+			VolumeID: volumeID,
+			NodeName: nodeName,
+		})
+	if err != nil {
+		msg := fmt.Sprintf("Failed to get the pod vmuuid annotation from the pod listener service. Error: %+v", err)
+		klog.Error(msg)
+		return "", err
+	}
+
+	klog.V(2).Infof("Got vmuuid: %s annotation from Pod Listener gRPC service", res.VmuuidAnnotation)
+	return res.VmuuidAnnotation, nil
+}
+
+// getDatacenterFromConfig gets the vcenter-datacenter where WCP PodVM cluster is deployed
+func getDatacenterFromConfig(cfg *config.Config) (map[string]string, error) {
+	vcdcMap := make(map[string]string)
+	vcdcListMap, err := getVCDatacentersFromConfig(cfg)
+	if err != nil {
+		return vcdcMap, err
+	}
+	if len(vcdcListMap) != 1 {
+		return vcdcMap, fmt.Errorf("Found more than one vCenter instance. WCP Cluster can be deployed in only one VC")
+	}
+
+	for vcHost, dcList := range vcdcListMap {
+		if len(dcList) > 1 {
+			return vcdcMap, fmt.Errorf("Found more than one datacenter instances: %+v for vcHost: %s. WCP Cluster can be deployed in only one datacenter", dcList, vcHost)
+		}
+		vcdcMap[vcHost] = dcList[0]
+	}
+	return vcdcMap, nil
+}
+
+// GetVCDatacenters returns list of datacenters for each vCenter that is registered
+func getVCDatacentersFromConfig(cfg *config.Config) (map[string][]string, error) {
+	var err error
+	vcdcMap := make(map[string][]string)
+	for key, value := range cfg.VirtualCenter {
+		dcList := strings.Split(value.Datacenters, ",")
+		for _, dc := range dcList {
+			dcMoID := strings.TrimSpace(dc)
+			if dcMoID != "" {
+				vcdcMap[key] = append(vcdcMap[key], dcMoID)
 			}
 		}
 	}
+	if len(vcdcMap) == 0 {
+		err = errors.New("Unable get vCenter datacenters from vsphere config")
+	}
+	return vcdcMap, err
+}
 
-	// Volume Name
-	volName := req.GetName()
-	if len(volName) == 0 {
-		msg := "Volume name is a required parameter."
-		klog.Error(msg)
-		return status.Error(codes.InvalidArgument, msg)
+/*
+ * getVMByInstanceUUIDInDatacenter gets the VM with the given instance UUID
+ * in datacenter specified using datacenter moref value.
+ */
+func getVMByInstanceUUIDInDatacenter(ctx context.Context,
+	vc *vsphere.VirtualCenter,
+	datacenter string,
+	vmInstanceUUID string) (*vsphere.VirtualMachine, error) {
+	var dc *vsphere.Datacenter
+	var vm *vsphere.VirtualMachine
+	dc = &vsphere.Datacenter{
+		Datacenter: object.NewDatacenter(vc.Client.Client,
+			types.ManagedObjectReference{
+				Type:  "Datacenter",
+				Value: datacenter,
+			}),
+		VirtualCenterHost: vc.Config.Host,
 	}
-	// Validate Volume Capabilities
-	volCaps := req.GetVolumeCapabilities()
-	if len(volCaps) == 0 {
-		return status.Error(codes.InvalidArgument, "Volume capabilities not provided")
+	// Get VM by UUID from datacenter
+	vm, err := dc.GetVirtualMachineByUUID(ctx, vmInstanceUUID, true)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to the VM from the VM Instance UUID: %s in datacenter: %+v with err: %+v", vmInstanceUUID, dc, err)
 	}
-	if !block.IsValidVolumeCapabilities(volCaps) {
-		return status.Error(codes.InvalidArgument, "Volume capabilities not supported")
-	}
+	return vm, nil
+}
 
-	// Validate volume size exists in spec
-	if req.GetCapacityRange() == nil || req.GetCapacityRange().RequiredBytes == 0 {
-		return status.Error(codes.InvalidArgument, "Volume size is a required parameter")
+// getPodListenerServicePort return the port to connect the Pod Listener gRPC service.
+// If environment variable X_CSI_POD_LISTENER_SERVICE_PORT is set and valid,
+// return the interval value read from enviroment variable
+// otherwise, use the default port
+func getPodListenerServicePort() int {
+	podListenerServicePort := defaultPodListenerServicePort
+	if v := os.Getenv("X_CSI_POD_LISTENER_SERVICE_PORT"); v != "" {
+		if value, err := strconv.Atoi(v); err == nil {
+			if value <= 0 {
+				klog.Warningf("Connecting to Pod Listener Service on port set in env variable X_CSI_POD_LISTENER_SERVICE_PORT %s is equal or less than 0, will use the default port %d", v, defaultPodListenerServicePort)
+			} else {
+				podListenerServicePort = value
+				klog.V(4).Infof("Connecting to Pod Listener Service on port %d", podListenerServicePort)
+			}
+		} else {
+			klog.Warningf("Connecting to Pod Listener Service on port set in env variable X_CSI_POD_LISTENER_SERVICE_PORT %s is invalid, will use the default port %d", v, defaultPodListenerServicePort)
+		}
 	}
-	return nil
+	return podListenerServicePort
 }
