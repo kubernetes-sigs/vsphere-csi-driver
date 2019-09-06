f04aaf334751dc4c8dd421299050e2d26770ea2f
Divyen Patel <divyenp@vmware.com>
2019-09-06 12:06:39 -0700

    bumping up taskClientVersion and using GetTaskInfo and GetTaskResult from govmomi repo

--- pkg/common/cns-lib/volume/util.go
+++ pkg/common/cns-lib/volume/util.go
@@ -17,52 +17,14 @@ package volume
 import (
 	"context"
 	"errors"
-	cnstypes "gitlab.eng.vmware.com/hatchway/govmomi/cns/types"
-	"gitlab.eng.vmware.com/hatchway/govmomi/object"
 	vimtypes "gitlab.eng.vmware.com/hatchway/govmomi/vim25/types"
 	"k8s.io/klog"
 	cnsvsphere "sigs.k8s.io/vsphere-csi-driver/pkg/common/cns-lib/vsphere"
 )
 
-// version and namespace constants for task client
 const (
-	// ClientVersion "6.9.0" is mapped to 'vsan.version.version11'
-	// This need to be changed if later VMODL version is bumped
-	taskClientVersion   = "6.9.0"
-	taskClientNamespace = "urn:vsan"
-
 	CNSVolumeResourceInUseFaultMessage = "The resource 'volume' is in use."
-	taskInfoEmptyErrMsg                = "TaskInfo is empty"
-	getVolOpResErrMsg                  = "Cannot get VolumeOperationResult"
 )
-
-// GetTaskResult gets the task result given a task info
-func GetTaskResult(ctx context.Context, taskInfo *vimtypes.TaskInfo) (cnstypes.BaseCnsVolumeOperationResult, error) {
-	if taskInfo == nil {
-		klog.Error(taskInfoEmptyErrMsg)
-		return nil, errors.New(taskInfoEmptyErrMsg)
-	}
-	volumeOperationBatchResult := taskInfo.Result.(cnstypes.CnsVolumeOperationBatchResult)
-	if &volumeOperationBatchResult == nil ||
-		volumeOperationBatchResult.VolumeResults == nil ||
-		len(volumeOperationBatchResult.VolumeResults) == 0 {
-		klog.Error(getVolOpResErrMsg)
-		return nil, errors.New(getVolOpResErrMsg)
-	}
-	return volumeOperationBatchResult.VolumeResults[0], nil
-}
-
-// GetTaskInfo gets the task info given a task
-func GetTaskInfo(ctx context.Context, task *object.Task) (*vimtypes.TaskInfo, error) {
-	task.Client().Version = taskClientVersion
-	task.Client().Namespace = taskClientNamespace
-	taskInfo, err := task.WaitForResult(ctx, nil)
-	if err != nil {
-		klog.Errorf("Failed to complete task. Task failed on wait with err: %v", err)
-		return nil, err
-	}
-	return taskInfo, nil
-}
 
 func validateManager(m *defaultManager) error {
 	if m.virtualCenter == nil {
