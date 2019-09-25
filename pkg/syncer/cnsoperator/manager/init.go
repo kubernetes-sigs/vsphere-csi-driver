f0853e7fca0e1350e513961db90f9c2983ebafa3
bdontu <bdontu@vmware.com>
2019-09-24 17:43:37 -0700

    Create CNS Operator for CNSNodeVMAttachment API using Operator SDK framework. Add dummy controller implementation to reconcile on CNSNodeVMAttachment API. Integrate CNS Operator with Syncer container.

---
+++ pkg/syncer/cnsoperator/manager/init.go
@@ -0,0 +1,79 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package manager
+
+import (
+	"fmt"
+
+	"k8s.io/klog"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
+	"sigs.k8s.io/vsphere-csi-driver/pkg/syncer/cnsoperator/apis"
+	"sigs.k8s.io/vsphere-csi-driver/pkg/syncer/cnsoperator/controller"
+)
+
+var (
+	// Use localhost and port for metrics
+	metricsHost       = "0.0.0.0"
+	metricsPort int32 = 8383
+)
+
+// InitCnsOperator initializes the Cns Operator
+func InitCnsOperator() error {
+	klog.V(2).Infof("Initializing CNS Operator")
+	// Get a config to talk to the apiserver
+	cfg, err := config.GetConfig()
+	if err != nil {
+		klog.Errorf("Failed to get Kubernetes config. Err: %+v", err)
+		return err
+	}
+
+	// Create a new operator to provide shared dependencies and start components
+	// Setting namespace to empty would let operator watch all namespaces.
+	mgr, err := manager.New(cfg, manager.Options{
+		Namespace:          "",
+		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
+	})
+	if err != nil {
+		klog.Errorf("Failed to create new Cns operator instance. Err: %+v", err)
+		return err
+	}
+
+	klog.V(2).Info("Registering Components for Cns Operator")
+
+	// Setup Scheme for all resources
+	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
+		klog.Errorf("Failed to set the scheme for Cns operator. Err: %+v", err)
+		return err
+	}
+
+	// Setup all Controllers
+	if err := controller.AddToManager(mgr); err != nil {
+		klog.Errorf("Failed to setup the controller for Cns operator. Err: %+v", err)
+		return err
+	}
+
+	klog.V(2).Info("Starting Cns Operator")
+
+	// Start the operator
+	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+		klog.Errorf("Failed to start Cns operator. Err: %+v", err)
+		return err
+	}
+	return nil
+}
