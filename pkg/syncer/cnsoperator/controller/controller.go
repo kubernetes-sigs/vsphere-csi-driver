3275e1aa87a871667b91fe9e7deb42f86f60224e
shahra <shahra@vmware.com>
2019-10-07 12:26:44 -0700

    Initialize Metadata syncer based on cluster flavor and refactor syncer types

--- pkg/syncer/cnsoperator/controller/controller.go
+++ pkg/syncer/cnsoperator/controller/controller.go
@@ -18,16 +18,17 @@ package controller
 
 import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	volumes "sigs.k8s.io/vsphere-csi-driver/pkg/common/cns-lib/volume"
 	"sigs.k8s.io/vsphere-csi-driver/pkg/syncer/types"
 )
 
 // AddToManagerFuncs is a list of functions to add all Controllers to the Manager
-var AddToManagerFuncs []func(manager.Manager, *types.ConfigInfo, *types.VirtualCenterTypes) error
+var AddToManagerFuncs []func(manager.Manager, *types.ConfigInfo, volumes.Manager) error
 
 // AddToManager adds all Controllers to the Manager
-func AddToManager(manager manager.Manager, configInfo *types.ConfigInfo, vcTypes *types.VirtualCenterTypes) error {
+func AddToManager(manager manager.Manager, configInfo *types.ConfigInfo, volumeManager volumes.Manager) error {
 	for _, f := range AddToManagerFuncs {
-		if err := f(manager, configInfo, vcTypes); err != nil {
+		if err := f(manager, configInfo, volumeManager); err != nil {
 			return err
 		}
 	}
