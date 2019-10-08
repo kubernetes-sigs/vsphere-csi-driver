346865b49e2924a878e88f0ea0acb2ba0041e7bb
Divyen Patel <divyenp@vmware.com>
2019-10-08 14:00:50 -0700

    removing hostNetwork setting from CSI controller and node pods

--- pkg/csi/service/node.go
+++ pkg/csi/service/node.go
@@ -376,17 +376,16 @@ func (s *service) NodeGetInfo(
 	ctx context.Context,
 	req *csi.NodeGetInfoRequest) (
 	*csi.NodeGetInfoResponse, error) {
-	nodeId, err := os.Hostname()
-	if err != nil {
-		return nil, status.Errorf(codes.Internal,
-			"Unable to retrieve Node ID, err: %s", err)
+	nodeId := os.Getenv("NODE_NAME")
+	if nodeId == "" {
+		return nil, status.Error(codes.Internal, "ENV NODE_NAME is not set")
 	}
 	var cfg *cnsconfig.Config
 	cfgPath = csictx.Getenv(ctx, cnsconfig.EnvCloudConfig)
 	if cfgPath == "" {
 		cfgPath = cnsconfig.DefaultCloudConfigPath
 	}
-	cfg, err = cnsconfig.GetCnsconfig(cfgPath)
+	cfg, err := cnsconfig.GetCnsconfig(cfgPath)
 	if err != nil {
 		klog.Errorf("Failed to read cnsconfig. Error: %v", err)
 		return nil, status.Errorf(codes.Internal, err.Error())
