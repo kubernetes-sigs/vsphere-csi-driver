d897d7fe1d99f1d0cd713a160c5727b7052f7868
chethanv28 <chethanv@vmware.com>
2019-11-18 10:52:54 -0800

    Cleanup files which are not needed for master branch

--- build_push.sh
+++
@@ -1,5 +0,0 @@
-make build-images && \
-	docker tag vsphere-csi-driver/vsphere-csi:latest docker.io/sandeeppissay/vsphere-csi:$1 && \
-	docker tag vsphere-csi-driver/syncer:latest docker.io/sandeeppissay/syncer:$1 && \
-        docker push docker.io/sandeeppissay/vsphere-csi:$1 && \
-	docker push docker.io/sandeeppissay/syncer:$1
