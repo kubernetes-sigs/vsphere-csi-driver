1248af6ecc4322d194d2697b9cf6625458fa2508
Chethan Venkatesh <chethanv@vmware.com>
2020-07-11 11:08:56 -0700

    Reduce e2e runtime for supervisor cluster

--- tests/e2e/data_persistence.go
+++ tests/e2e/data_persistence.go
@@ -183,12 +183,10 @@ var _ = ginkgo.Describe("Data Persistence", func() {
 		err = framework.DeletePodWithWait(f, client, pod)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		if supervisorCluster {
-			ginkgo.By("Wait for 3 minutes for the pod to get terminated successfully")
-			time.Sleep(supervisorClusterOperationsTimeout)
 			ginkgo.By(fmt.Sprintf("Verify volume: %s is detached from PodVM with vmUUID: %s", pv.Spec.CSI.VolumeHandle, vmUUID))
 			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
-			_, err := e2eVSphere.getVMByUUID(ctx, vmUUID)
+			_, err := e2eVSphere.getVMByUUIDWithWait(ctx, vmUUID, supervisorClusterOperationsTimeout)
 			gomega.Expect(err).To(gomega.HaveOccurred(), fmt.Sprintf("PodVM with vmUUID: %s still exists. So volume: %s is not detached from the PodVM", vmUUID, pv.Spec.CSI.VolumeHandle))
 		} else {
 			ginkgo.By("Verify volume is detached from the node")
@@ -237,12 +235,10 @@ var _ = ginkgo.Describe("Data Persistence", func() {
 		err = framework.DeletePodWithWait(f, client, pod)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		if supervisorCluster {
-			ginkgo.By("Wait for 3 minutes for the pod to get terminated successfully")
-			time.Sleep(supervisorClusterOperationsTimeout)
 			ginkgo.By(fmt.Sprintf("Verify volume: %s is detached from PodVM with vmUUID: %s", pv.Spec.CSI.VolumeHandle, vmUUID))
 			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
-			_, err := e2eVSphere.getVMByUUID(ctx, vmUUID)
+			_, err := e2eVSphere.getVMByUUIDWithWait(ctx, vmUUID, supervisorClusterOperationsTimeout)
 			gomega.Expect(err).To(gomega.HaveOccurred(), fmt.Sprintf("PodVM with vmUUID: %s still exists. So volume: %s is not detached from the PodVM", vmUUID, pv.Spec.CSI.VolumeHandle))
 		} else {
 			ginkgo.By("Verify volume is detached from the node")
@@ -368,13 +364,11 @@ var _ = ginkgo.Describe("Data Persistence", func() {
 		ginkgo.By("Deleting the pod")
 		framework.DeletePodWithWait(f, client, pod)
 
-		ginkgo.By("Wait for 3 minutes for the pod to get terminated successfully")
-		time.Sleep(supervisorClusterOperationsTimeout)
 		ginkgo.By(fmt.Sprintf("Verify volume: %s is detached from PodVM with vmUUID: %s", pv.Spec.CSI.VolumeHandle, vmUUID))
 		ctx, cancel = context.WithCancel(context.Background())
 		defer cancel()
 
-		_, err = e2eVSphere.getVMByUUID(ctx, vmUUID)
+		_, err = e2eVSphere.getVMByUUIDWithWait(ctx, vmUUID, supervisorClusterOperationsTimeout)
 		gomega.Expect(err).To(gomega.HaveOccurred(), fmt.Sprintf("PodVM with vmUUID: %s still exists. So volume: %s is not detached from the PodVM", vmUUID, pv.Spec.CSI.VolumeHandle))
 
 		ginkgo.By("Creating a new pod using the same volume")
@@ -400,12 +394,11 @@ var _ = ginkgo.Describe("Data Persistence", func() {
 
 		ginkgo.By("Deleting the pod")
 		framework.DeletePodWithWait(f, client, pod)
-		ginkgo.By("Wait for 3 minutes for the pod to get terminated successfully")
-		time.Sleep(supervisorClusterOperationsTimeout)
+
 		ginkgo.By(fmt.Sprintf("Verify volume: %s is detached from PodVM with vmUUID: %s", pv.Spec.CSI.VolumeHandle, vmUUID))
 		ctx, cancel = context.WithCancel(context.Background())
 		defer cancel()
-		_, err = e2eVSphere.getVMByUUID(ctx, vmUUID)
+		_, err = e2eVSphere.getVMByUUIDWithWait(ctx, vmUUID, supervisorClusterOperationsTimeout)
 		gomega.Expect(err).To(gomega.HaveOccurred(), fmt.Sprintf("PodVM with vmUUID: %s still exists. So volume: %s is not detached from the PodVM", vmUUID, pv.Spec.CSI.VolumeHandle))
 
 		ginkgo.By("Deleting the PV Claim")
