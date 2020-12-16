cb6d12777b96cc6c700fcff2c1b02aebc7ce647a
Chethan Venkatesh <chethanv@vmware.com>
2020-12-16 10:46:53 -0800

    CnsFileAccessConfig controller implementation

---
+++ pkg/syncer/cnsoperator/util/util.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"context"
+
+	v1 "k8s.io/api/core/v1"
+	k8stypes "k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/vsphere-csi-driver/pkg/csi/service/logger"
+)
+
+// GetVolumeID gets the volume ID from the PV that is bound to PVC by pvcName
+func GetVolumeID(ctx context.Context, client client.Client, pvcName string, namespace string) (string, error) {
+	log := logger.GetLogger(ctx)
+	// Get PVC by pvcName from namespace
+	pvc := &v1.PersistentVolumeClaim{}
+	// TODO:
+	// Enhancements to use informers instead of API server invocations is tracked here:
+	// https://github.com/kubernetes-sigs/vsphere-csi-driver/issues/599
+	err := client.Get(ctx, k8stypes.NamespacedName{Name: pvcName, Namespace: namespace}, pvc)
+	if err != nil {
+		log.Errorf("failed to get PVC with volumename: %q on namespace: %q. Err: %+v",
+			pvcName, namespace, err)
+		return "", err
+	}
+
+	// Get PV by name
+	pv := &v1.PersistentVolume{}
+	err = client.Get(ctx, k8stypes.NamespacedName{Name: pvc.Spec.VolumeName, Namespace: ""}, pv)
+	if err != nil {
+		log.Errorf("failed to get PV with name: %q for PVC: %q. Err: %+v",
+			pvc.Spec.VolumeName, pvcName, err)
+		return "", err
+	}
+	return pv.Spec.CSI.VolumeHandle, nil
+}
