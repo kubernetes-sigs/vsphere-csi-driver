903789c7fa85b025e7e22083c7c08013ec788990
BaluDontu <bdontu@vmware.com>
2021-03-17 12:55:14 -0700

    Not run auth manager for file volumes if vSAN file services is not enabled

--- pkg/csi/service/common/authmanager.go
+++ pkg/csi/service/common/authmanager.go
@@ -201,11 +201,17 @@ func GenerateDatastoreMapForBlockVolumes(ctx context.Context, vc *vsphere.Virtua
 // It will return datastores which has the privileges for creating file volume
 func GenerateDatastoreMapForFileVolumes(ctx context.Context, vc *vsphere.VirtualCenter) (map[string]*cnsvsphere.DatastoreInfo, error) {
 	log := logger.GetLogger(ctx)
+	dsURLToInfoMap := make(map[string]*cnsvsphere.DatastoreInfo)
 	// get all vSAN datastores from VC
 	vsanDsURLToInfoMap, err := vc.GetVsanDatastores(ctx)
 	if err != nil {
 		log.Errorf("failed to get vSAN datastores with error %+v", err)
 		return nil, err
+	}
+	// Return empty map if no vSAN datastores are found.
+	if len(vsanDsURLToInfoMap) == 0 {
+		log.Debug("No vSAN datastores found")
+		return dsURLToInfoMap, nil
 	}
 	var allvsanDatastoreUrls []string
 	for dsURL := range vsanDsURLToInfoMap {
@@ -217,7 +223,6 @@ func GenerateDatastoreMapForFileVolumes(ctx context.Context, vc *vsphere.Virtual
 		return nil, err
 	}
 
-	dsURLToInfoMap := make(map[string]*cnsvsphere.DatastoreInfo)
 	for dsURL, dsInfo := range vsanDsURLToInfoMap {
 		if val, ok := fsEnabledMap[dsURL]; ok {
 			if val {
@@ -383,6 +388,14 @@ func getDsToFileServiceEnabledMap(ctx context.Context, vc *vsphere.VirtualCenter
 		if err != nil {
 			log.Errorf("failed to get the vsan cluster config. error: %+v", err)
 			return nil, err
+		}
+		if !(*config.Enabled) {
+			log.Debugf("cluster: %+v is a non-vSAN cluster. Skipping this cluster", cluster)
+			continue
+		} else if config.FileServiceConfig == nil {
+			log.Debugf("VsanClusterGetConfig.FileServiceConfig is empty. Skipping this cluster: %+v with config: %+v",
+				cluster, config)
+			continue
 		}
 		log.Debugf("cluster: %+v has vSAN file services enabled: %t", cluster, config.FileServiceConfig.Enabled)
 		var dsList []vim25types.ManagedObjectReference
