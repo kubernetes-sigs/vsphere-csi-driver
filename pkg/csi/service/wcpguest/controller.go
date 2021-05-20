f9f22fa8ecad81a0086b4f7de947cf20cf4d5700
Divyen Patel <divyenp@vmware.com>
2021-05-20 16:55:38 -0700

    using logger.GetNewContextWithLogger instead of creating context using context.WithCancel(context.Background())

--- pkg/csi/service/wcpguest/controller.go
+++ pkg/csi/service/wcpguest/controller.go
@@ -84,11 +84,7 @@ func New() csitypes.CnsController {
 
 // Init is initializing controller struct
 func (c *controller) Init(config *cnsconfig.Config, version string) error {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	ctx = logger.NewContextWithLogger(ctx)
-	log := logger.GetLogger(ctx)
-
+	ctx, log := logger.GetNewContextWithLogger()
 	log.Infof("Initializing WCPGC CSI controller")
 	var err error
 	// connect to the CSI controller in supervisor cluster
