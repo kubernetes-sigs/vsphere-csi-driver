78e91de2b837166d005e5df047fcb9c881dd79fb
Zhelong <zpan@vmware.com>
2021-06-21 08:54:32 -0700

    Clean up white spaces in apis

--- pkg/apis/storagepool/apis.go
+++ pkg/apis/storagepool/apis.go
@@ -15,7 +15,8 @@ limitations under the License.
 */
 
 // Generate deepcopy for apis
-//go:generate go run ../../vendor/k8s.io/code-generator/cmd/deepcopy-gen/main.go -O zz_generated.deepcopy -i ./... -h ../../hack/boilerplate.go.txt
+// go:generate go run ../../vendor/k8s.io/code-generator/cmd/deepcopy-gen/main.go
+//    -O zz_generated.deepcopy -i ./... -h ../../hack/boilerplate.go.txt
 
 // Package apis contains Kubernetes API groups.
 package apis
@@ -24,10 +25,11 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-// AddToSchemes may be used to add all resources defined in the project to a Scheme
+// AddToSchemes may be used to add all resources defined in the project to
+// a Scheme.
 var AddToSchemes runtime.SchemeBuilder
 
-// AddToScheme adds all Resources to the Scheme
+// AddToScheme adds all Resources to the Scheme.
 func AddToScheme(s *runtime.Scheme) error {
 	return AddToSchemes.AddToScheme(s)
 }
