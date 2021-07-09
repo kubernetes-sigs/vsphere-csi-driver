91d7fe49d29681f56a6fb94ac6177d2d91e5a8e7
Zhelong <83981483+zhelongp@users.noreply.github.com>
2021-07-09 13:32:54 -0700

    Use LogNewErrorf in cns-lib util (#1103)

--- pkg/common/cns-lib/vsphere/utils.go
+++ pkg/common/cns-lib/vsphere/utils.go
@@ -199,11 +199,10 @@ func GetVirtualCenterConfig(ctx context.Context, cfg *config.Config) (*VirtualCe
 	for idx := range vcConfig.TargetvSANFileShareDatastoreURLs {
 		vcConfig.TargetvSANFileShareDatastoreURLs[idx] = strings.TrimSpace(vcConfig.TargetvSANFileShareDatastoreURLs[idx])
 		if vcConfig.TargetvSANFileShareDatastoreURLs[idx] == "" {
-			return nil, errors.New("invalid datastore URL specified in targetvSANFileShareDatastoreURLs")
+			return nil, logger.LogNewError(log, "invalid datastore URL specified in targetvSANFileShareDatastoreURLs")
 		}
 		if !strings.HasPrefix(vcConfig.TargetvSANFileShareDatastoreURLs[idx], "ds:///vmfs/volumes/vsan:") {
-			err = errors.New("non vSAN datastore specified for targetvSANFileShareDatastoreURLs")
-			return nil, err
+			return nil, logger.LogNewError(log, "non vSAN datastore specified for targetvSANFileShareDatastoreURLs")
 		}
 	}
 	return vcConfig, nil
@@ -434,9 +433,7 @@ func IsvSphereVersion70U3orAbove(ctx context.Context, aboutInfo types.AboutInfo)
 	if len(version) >= 3 {
 		vSphereVersionInt, err := strconv.Atoi(version[0:3])
 		if err != nil {
-			msg := fmt.Sprintf("error while converting version %q to integer, err %+v", version, err)
-			log.Errorf(msg)
-			return false, errors.New(msg)
+			return false, logger.LogNewErrorf(log, "error while converting version %q to integer, err %+v", version, err)
 		}
 		// Check if the current vSphere version is 7.0.3 or higher.
 		if vSphereVersionInt >= VSphere70u3Version {
