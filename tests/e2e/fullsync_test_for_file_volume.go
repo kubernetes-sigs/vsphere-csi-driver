6362bbdb6721fb735689bf47b8b1555e9ee27cac
Zhelong <83981483+zhelongp@users.noreply.github.com>
2021-07-13 09:06:30 -0700

    Clean up white spaces in fullsync_test_for_file_volume (#1122)

--- tests/e2e/fullsync_test_for_file_volume.go
+++ tests/e2e/fullsync_test_for_file_volume.go
@@ -36,16 +36,15 @@ import (
 	fpv "k8s.io/kubernetes/test/e2e/framework/pv"
 )
 
-/*
-   Tests to verify Full Sync .
-
-   Test 1) Verify CNS volume is created after full sync when pv entry is present.
-   Test 2) Verify labels are created in CNS after updating pvc and/or pv with new labels.
-   Test 3) Verify CNS volume is deleted after full sync when pv entry is delete
-
-   Cleanup
-   - Delete PVC and StorageClass and verify volume is deleted from CNS.
-*/
+// Tests to verify Full Sync .
+//
+// Test 1) Verify CNS volume is created after full sync when pv entry is present.
+// Test 2) Verify labels are created in CNS after updating pvc and/or pv with
+//         new labels.
+// Test 3) Verify CNS volume is deleted after full sync when pv entry is delete.
+//
+// Cleanup
+// - Delete PVC and StorageClass and verify volume is deleted from CNS.
 
 var _ bool = ginkgo.Describe("[csi-file-vanilla] Full sync test for file volume", func() {
 	f := framework.NewDefaultFramework("e2e-full-sync-test-file-volume")
@@ -88,31 +87,30 @@ var _ bool = ginkgo.Describe("[csi-file-vanilla] Full sync test for file volume"
 			ginkgo.By(fmt.Sprintln("Starting vsan-health on the vCenter host"))
 			err := invokeVCenterServiceControl(startOperation, vsanhealthServiceName, vcAddress)
 			gomega.Expect(err).NotTo(gomega.HaveOccurred())
-			ginkgo.By(fmt.Sprintf("Sleeping for %v seconds to allow vsan-health to come up again", vsanHealthServiceWaitTime))
+			ginkgo.By(fmt.Sprintf("Sleeping for %v seconds to allow vsan-health to come up again",
+				vsanHealthServiceWaitTime))
 			time.Sleep(time.Duration(vsanHealthServiceWaitTime) * time.Second)
 		}
 	})
 
-	/*
-				Verify fullsync is able to update label of pv and pvc
-					1. Create StorageClass with fsType as "nfs4"
-		            2. Create a PVC with "ReadWriteMany" using the SC from above
-		            3. Wait for PVC to be Bound
-		            4. Get the VolumeID from PV
-		            5. stop vSan health service
-		            6. Update label for pvc
-		            7. Update label for pv
-		            8. start vSan health service
-		            9. wait for FullSync to finish
-		            10. verify pv and pvc label has been updated
-		            11. Delete PVC
-		            12. Delete Storage class
-	*/
+	// Verify fullsync is able to update label of pv and pvc.
+	// 1. Create StorageClass with fsType as "nfs4".
+	// 2. Create a PVC with "ReadWriteMany" using the SC from above.
+	// 3. Wait for PVC to be Bound.
+	// 4. Get the VolumeID from PV.
+	// 5. Stop vSan health service.
+	// 6. Update label for pvc.
+	// 7. Update label for pv.
+	// 8. Start vSan health service.
+	// 9. Wait for FullSync to finish.
+	// 10. Verify pv and pvc label has been updated.
+	// 11. Delete PVC.
+	// 12. Delete Storage class.
 	ginkgo.It("verify labels are created in CNS after updating pvc and/or pv with new labels for file volume", func() {
 		ginkgo.By("Invoking test to verify labels creation")
 		scParameters := make(map[string]string)
 		scParameters[scParamFsType] = nfs4FSType
-		// Create Storage class and PVC
+		// Create Storage class and PVC.
 		ginkgo.By(fmt.Sprintf("Creating Storage Class with %q", nfs4FSType))
 		var storageclass *storagev1.StorageClass
 		var pvc *v1.PersistentVolumeClaim
@@ -120,7 +118,8 @@ var _ bool = ginkgo.Describe("[csi-file-vanilla] Full sync test for file volume"
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
 
-		storageclass, pvc, err = createPVCAndStorageClass(client, namespace, nil, scParameters, "", nil, "", false, v1.ReadWriteMany)
+		storageclass, pvc, err = createPVCAndStorageClass(client,
+			namespace, nil, scParameters, "", nil, "", false, v1.ReadWriteMany)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		defer func() {
 			err = client.StorageV1().StorageClasses().Delete(ctx, storageclass.Name, *metav1.NewDeleteOptions(0))
@@ -128,7 +127,8 @@ var _ bool = ginkgo.Describe("[csi-file-vanilla] Full sync test for file volume"
 		}()
 
 		ginkgo.By(fmt.Sprintf("Waiting for claim %s to be in bound phase", pvc.Name))
-		pvs, err := fpv.WaitForPVClaimBoundPhase(client, []*v1.PersistentVolumeClaim{pvc}, framework.ClaimProvisionTimeout)
+		pvs, err := fpv.WaitForPVClaimBoundPhase(client,
+			[]*v1.PersistentVolumeClaim{pvc}, framework.ClaimProvisionTimeout)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		gomega.Expect(pvs).NotTo(gomega.BeEmpty())
 		pv := pvs[0]
@@ -152,7 +152,8 @@ var _ bool = ginkgo.Describe("[csi-file-vanilla] Full sync test for file volume"
 			}
 		}()
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-		ginkgo.By(fmt.Sprintf("Sleeping for %v seconds to allow vsan-health to completely shutdown", vsanHealthServiceWaitTime))
+		ginkgo.By(fmt.Sprintf("Sleeping for %v seconds to allow vsan-health to completely shutdown",
+			vsanHealthServiceWaitTime))
 		time.Sleep(time.Duration(vsanHealthServiceWaitTime) * time.Second)
 
 		labels := make(map[string]string)
@@ -180,12 +181,15 @@ var _ bool = ginkgo.Describe("[csi-file-vanilla] Full sync test for file volume"
 		ginkgo.By(fmt.Sprintf("Sleeping for %v seconds to allow full sync finish", fullSyncWaitTime))
 		time.Sleep(time.Duration(fullSyncWaitTime) * time.Second)
 
-		ginkgo.By(fmt.Sprintf("Waiting for labels %+v to be updated for pvc %s in namespace %s", labels, pvc.Name, pvc.Namespace))
-		err = e2eVSphere.waitForLabelsToBeUpdated(pv.Spec.CSI.VolumeHandle, labels, string(cnstypes.CnsKubernetesEntityTypePVC), pvc.Name, pvc.Namespace)
+		ginkgo.By(fmt.Sprintf("Waiting for labels %+v to be updated for pvc %s in namespace %s",
+			labels, pvc.Name, pvc.Namespace))
+		err = e2eVSphere.waitForLabelsToBeUpdated(pv.Spec.CSI.VolumeHandle,
+			labels, string(cnstypes.CnsKubernetesEntityTypePVC), pvc.Name, pvc.Namespace)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 		ginkgo.By(fmt.Sprintf("Waiting for labels %+v to be updated for pv %s", labels, pv.Name))
-		err = e2eVSphere.waitForLabelsToBeUpdated(pv.Spec.CSI.VolumeHandle, labels, string(cnstypes.CnsKubernetesEntityTypePV), pv.Name, pv.Namespace)
+		err = e2eVSphere.waitForLabelsToBeUpdated(pv.Spec.CSI.VolumeHandle,
+			labels, string(cnstypes.CnsKubernetesEntityTypePV), pv.Name, pv.Namespace)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 	})
