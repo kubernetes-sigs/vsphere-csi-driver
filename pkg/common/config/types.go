a4395f82234c3a9699a6982cf6d4cfb261ff474d
Shalini Bhaskara <sbhaskara@vmware.com>
2021-12-14 15:31:48 +0530

    Take user input for topology upgrade

--- pkg/common/config/types.go
+++ pkg/common/config/types.go
@@ -88,6 +88,8 @@ type Config struct {
 		// Maximum number of categories allowed is 5.
 		TopologyCategories string `gcfg:"topology-categories"`
 	}
+
+	TopologyCategory map[string]*TopologyCategoryInfo
 }
 
 // ConfigurationInfo is a struct that used to capture config param details
@@ -99,6 +101,11 @@ type ConfigurationInfo struct {
 type FeatureStatesConfigInfo struct {
 	Name      string
 	Namespace string
+}
+
+// TopologyCategoryInfo contains metadata for the Zone and Region parameters under Labels section.
+type TopologyCategoryInfo struct {
+	Label string `gcfg:"label"`
 }
 
 // NetPermissionConfig consists of information used to restrict the
