258dda7448d6d15dd963bdc4d1cd4026694a8b91
sashrith <sashrith@vmware.com>
2022-02-15 00:44:30 +0530

    return false if FSS is not found instead of failing in isCsiFssEnabled() (#1550)

--- tests/e2e/util.go
+++ tests/e2e/util.go
@@ -4096,7 +4096,8 @@ func isCsiFssEnabled(ctx context.Context, client clientset.Interface, namespace 
 			}
 		}
 	}
-	gomega.Expect(fssFound).To(
-		gomega.BeTrue(), "FSS %s not found in the %s configmap in namespace %s", fss, csiFssCM, namespace)
+	if !fssFound {
+		framework.Logf("FSS %s not found in the %s configmap in namespace %s", fss, csiFssCM, namespace)
+	}
 	return false
 }
