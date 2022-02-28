b7b046a03c2db7e233701bd742f6ea5d99b4a049
marunachalam <67798021+marunachalam@users.noreply.github.com>
2022-02-28 12:28:56 +0530

    VMC VC Cert rotate scenario (#1508)
    * rebased the pr will master
    
    * Addressed review comments

---
+++ tests/e2e/vmc_vc_cert_rotate.go
@@ -0,0 +1,212 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+	storagev1 "k8s.io/api/storage/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/kubernetes/test/e2e/framework"
+	fnodes "k8s.io/kubernetes/test/e2e/framework/node"
+	fss "k8s.io/kubernetes/test/e2e/framework/statefulset"
+)
+
+var _ = ginkgo.Describe("VMC VC Cert Rotate", func() {
+	f := framework.NewDefaultFramework("vmc-cert-rotate")
+	var (
+		client            clientset.Interface
+		namespace         string
+		svNamespace       string
+		scParameters      map[string]string
+		storagePolicyName string
+		manifestPath      = "tests/e2e/testing-manifests/statefulset/nginx"
+		refreshToken      string
+		orgID             string
+		sddcID            string
+	)
+
+	ginkgo.BeforeEach(func() {
+		client = f.ClientSet
+		namespace = getNamespaceToRunTests(f)
+		svcClient, svNamespace = getSvcClientAndNamespace()
+		nodeList, err := fnodes.GetReadySchedulableNodes(f.ClientSet)
+		framework.ExpectNoError(err, "Unable to find ready and schedulable Node")
+		storagePolicyName = GetAndExpectStringEnvVar(envStoragePolicyNameForSharedDatastores)
+		if !(len(nodeList.Items) > 0) {
+			framework.Failf("Unable to find ready and schedulable Node")
+		}
+		bootstrap()
+		setResourceQuota(svcClient, svNamespace, rqLimit)
+		scParameters = make(map[string]string)
+		storagePolicyName = GetAndExpectStringEnvVar(envStoragePolicyNameForSharedDatastores)
+	})
+	ginkgo.AfterEach(func() {
+		svcClient, svNamespace := getSvcClientAndNamespace()
+		setResourceQuota(svcClient, svNamespace, defaultrqLimit)
+	})
+
+	/*
+		Test to validate the tkg cluster post vc cert rotation in vmc env
+		Steps
+			1. Create statefulset pods with 3 replicas
+			2. Execute the RTS script to perform cert rotation
+			3. Scale Up the statefulset pods and verify volumes are attached to the node
+			5. Scale Down the statefulset pods and verify volumes are detached from the node
+			6. Delete the statefulset pods and PVC's
+	*/
+
+	ginkgo.It("[vmc] VC Cert Rotate in VMC", func() {
+		var sc *storagev1.StorageClass
+		var err error
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		refreshToken = os.Getenv("REFRESH_TOKEN")
+		if refreshToken == "" {
+			ginkgo.Skip("Env REFRESH_TOKEN is missing")
+		}
+		orgID = os.Getenv("ORG_ID")
+		if orgID == "" {
+			ginkgo.Skip("Env ORG_ID is missing")
+		}
+		sddcID = os.Getenv("SDDC_ID")
+		if orgID == "" {
+			ginkgo.Skip("Env SDDC_ID is missing")
+		}
+
+		ginkgo.By("Creating StorageClass for Statefulset")
+		scParameters[svStorageClassName] = storagePolicyName
+		scSpec := getVSphereStorageClassSpec(defaultNginxStorageClassName, scParameters, nil, "", "", false)
+		sc, err = client.StorageV1().StorageClasses().Create(ctx, scSpec, metav1.CreateOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		defer func() {
+			err := client.StorageV1().StorageClasses().Delete(ctx, sc.Name, *metav1.NewDeleteOptions(0))
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		}()
+
+		ginkgo.By("Creating statefulset")
+		statefulset := fss.CreateStatefulSet(client, manifestPath, namespace)
+		defer func() {
+			ginkgo.By(fmt.Sprintf("Deleting all statefulsets in namespace: %v", namespace))
+			fss.DeleteAllStatefulSets(client, namespace)
+		}()
+		replicas := *(statefulset.Spec.Replicas)
+		// Waiting for pods status to be Ready.
+		fss.WaitForStatusReadyReplicas(client, statefulset, replicas)
+		gomega.Expect(fss.CheckMount(client, statefulset, mountPath)).NotTo(gomega.HaveOccurred())
+		ssPodsBeforeScaleUp := fss.GetPodList(client, statefulset)
+		gomega.Expect(ssPodsBeforeScaleUp.Items).NotTo(gomega.BeEmpty(),
+			fmt.Sprintf("Unable to get list of Pods from the Statefulset: %v", statefulset.Name))
+		gomega.Expect(len(ssPodsBeforeScaleUp.Items) == int(replicas)).To(gomega.BeTrue(),
+			"Number of Pods in the statefulset should match with number of replicas")
+
+		// Verify volumes are attached to the nodes.
+		for _, sspod := range ssPodsBeforeScaleUp.Items {
+			_, err := client.CoreV1().Pods(namespace).Get(ctx, sspod.Name, metav1.GetOptions{})
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			for _, volumespec := range sspod.Spec.Volumes {
+				if volumespec.PersistentVolumeClaim != nil {
+					pv := getPvFromClaim(client, statefulset.Namespace, volumespec.PersistentVolumeClaim.ClaimName)
+					ginkgo.By("Verify CnsNodeVmAttachment CRD is created")
+					volumeID := pv.Spec.CSI.VolumeHandle
+					// svcPVCName refers to PVC Name in the supervisor cluster.
+					svcPVCName := volumeID
+					volumeID = getVolumeIDFromSupervisorCluster(svcPVCName)
+					gomega.Expect(volumeID).NotTo(gomega.BeEmpty())
+					verifyCRDInSupervisor(ctx, f, sspod.Spec.NodeName+"-"+svcPVCName,
+						crdCNSNodeVMAttachment, crdVersion, crdGroup, true)
+
+					ginkgo.By(fmt.Sprintf("Verify volume: %s is attached to the node: %s",
+						pv.Spec.CSI.VolumeHandle, sspod.Spec.NodeName))
+					var vmUUID string
+					vmUUID, err = getVMUUIDFromNodeName(sspod.Spec.NodeName)
+					gomega.Expect(err).NotTo(gomega.HaveOccurred())
+					isDiskAttached, err := e2eVSphere.isVolumeAttachedToVM(client, volumeID, vmUUID)
+					gomega.Expect(err).NotTo(gomega.HaveOccurred())
+					gomega.Expect(isDiskAttached).To(gomega.BeTrue(),
+						fmt.Sprintf("Volume is not attached to the node, %s", vmUUID))
+
+					ginkgo.By(fmt.Sprintf("Verify volume: %s is attached to the node: %s",
+						pv.Spec.CSI.VolumeHandle, sspod.Spec.NodeName))
+					verifyIsAttachedInSupervisor(ctx, f, sspod.Spec.NodeName+"-"+pv.Spec.CSI.VolumeHandle,
+						crdVersion, crdGroup)
+
+				}
+			}
+		}
+
+		ginkgo.By("Rotating VC cert")
+		authToken := getAuthToken(refreshToken)
+		framework.Logf("authtoken %s ", authToken)
+		taskID := rotateVCCertinVMC(authToken, orgID, sddcID)
+		err = getTaskStatus(authToken, orgID, taskID)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		ginkgo.By(fmt.Sprintf("Scaling up statefulsets to number of Replica: %v", replicas+5))
+		_, scaleupErr := fss.Scale(client, statefulset, replicas+5)
+		gomega.Expect(scaleupErr).NotTo(gomega.HaveOccurred())
+		fss.WaitForStatusReplicas(client, statefulset, replicas+5)
+		fss.WaitForStatusReadyReplicas(client, statefulset, replicas+5)
+		ssPodsAfterScaleUp := fss.GetPodList(client, statefulset)
+		gomega.Expect(ssPodsAfterScaleUp.Items).NotTo(gomega.BeEmpty(),
+			fmt.Sprintf("Unable to get list of Pods from the Statefulset: %v", statefulset.Name))
+		gomega.Expect(len(ssPodsAfterScaleUp.Items) == int(replicas+5)).To(gomega.BeTrue(),
+			"Number of Pods in the statefulset should match with number of replicas")
+
+		fss.WaitForStatusReadyReplicas(client, statefulset, replicas+5)
+		gomega.Expect(fss.CheckMount(client, statefulset, mountPath)).NotTo(gomega.HaveOccurred())
+		ssPodsAfterScaleUp = fss.GetPodList(client, statefulset)
+		gomega.Expect(ssPodsAfterScaleUp.Items).NotTo(gomega.BeEmpty(),
+			fmt.Sprintf("Unable to get list of Pods from the Statefulset: %v", statefulset.Name))
+		gomega.Expect(len(ssPodsAfterScaleUp.Items) == int(replicas+5)).To(gomega.BeTrue(),
+			"Number of Pods in the statefulset should match with number of replicas")
+
+		ginkgo.By(fmt.Sprintf("Scaling down statefulsets to number of Replica: %v", 0))
+		_, scaledownErr := fss.Scale(client, statefulset, 0)
+		gomega.Expect(scaledownErr).NotTo(gomega.HaveOccurred())
+		fss.WaitForStatusReadyReplicas(client, statefulset, 0)
+		ssPodsAfterScaleDown := fss.GetPodList(client, statefulset)
+		gomega.Expect(len(ssPodsAfterScaleDown.Items) == int(0)).To(gomega.BeTrue(),
+			"Number of Pods in the statefulset should match with number of replicas")
+
+		ginkgo.By("Verify Volumes are detached from Nodes after Statefulsets is scaled down")
+		for _, sspod := range ssPodsAfterScaleUp.Items {
+			_, err := client.CoreV1().Pods(namespace).Get(ctx, sspod.Name, metav1.GetOptions{})
+			if err != nil {
+				gomega.Expect(apierrs.IsNotFound(err), gomega.BeTrue())
+				for _, volumespec := range sspod.Spec.Volumes {
+					if volumespec.PersistentVolumeClaim != nil {
+						pv := getPvFromClaim(client, statefulset.Namespace, volumespec.PersistentVolumeClaim.ClaimName)
+						ctx, cancel := context.WithCancel(context.Background())
+						defer cancel()
+						verifyIsDetachedInSupervisor(ctx, f, sspod.Spec.NodeName+"-"+pv.Spec.CSI.VolumeHandle,
+							crdVersion, crdGroup)
+					}
+				}
+			}
+		}
+
+	})
+
+})
