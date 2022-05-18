80a7c6057a92c5e6e799524ca549a83afd769a49
sipriyaa <sipriya@vmware.com>
2022-05-18 13:13:51 +0530

    Extra arg added podname in nimbus cmd for running power-on/off on esxi

--- tests/e2e/nimbus_utils.go
+++ tests/e2e/nimbus_utils.go
@@ -34,18 +34,20 @@ type TestbedBasicInfo struct {
 	vcIp     string
 	vcVmName string
 	esxHosts []map[string]string
+	podname  string
 }
 
 var tbinfo TestbedBasicInfo
 
 //vMPowerMgmt power on/off given nimbus VMs (space separated list)
-func vMPowerMgmt(user string, location string, hostList string, shouldBePoweredOn bool) error {
+func vMPowerMgmt(user string, location string, podname string, hostList string, shouldBePoweredOn bool) error {
 	var err error
 	op := "off"
 	if shouldBePoweredOn {
 		op = "on"
 	}
-	nimbusCmd := fmt.Sprintf("USER=%s /mts/git/bin/nimbus-ctl --nimbusLocation %s %s %s", user, location, op, hostList)
+	nimbusCmd := fmt.Sprintf("USER=%s /mts/git/bin/nimbus-ctl --nimbusLocation %s --nimbus %s %s %s", user,
+		location, podname, op, hostList)
 	framework.Logf("Running command: %s", nimbusCmd)
 	cmd := exec.Command("/bin/bash", "-c", nimbusCmd)
 	err = cmd.Start()
@@ -90,6 +92,7 @@ func readVcEsxIpsViaTestbedInfoJson(filePath string) {
 	tbinfo.name = tb["name"].(string)
 	tbinfo.user = tb["user_name"].(string)
 	tbinfo.location = tb["nimbusLocation"].(string)
+	tbinfo.podname = tb["podname"].(string)
 
 	framework.Logf("Basic testbed info:\n%s\n", spew.Sdump(tbinfo))
 }
