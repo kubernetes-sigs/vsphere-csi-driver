68375f91c0105b6c9d3739b12729575ae70c95cc
Divyen Patel <divyenp@vmware.com>
2022-06-03 07:51:54 -0700

    fixes to run prow tests

--- hack/check-staticcheck.sh
+++ hack/check-staticcheck.sh
@@ -22,7 +22,7 @@ set -o pipefail
 # script is located.
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-go get honnef.co/go/tools/cmd/staticcheck@v0.2.0
+go install honnef.co/go/tools/cmd/staticcheck@2022.1.1
 
 # shellcheck disable=SC2046
 # shellcheck disable=SC1083
