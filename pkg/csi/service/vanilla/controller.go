ab979124c2cc8c59cd894f7f1a7b5f8bc417532c
Liping Xue <lipingx@vmware.com>
2022-08-25 14:56:45 -0700

    Improve logs in authmanager.

--- pkg/csi/service/vanilla/controller.go
+++ pkg/csi/service/vanilla/controller.go
@@ -565,6 +565,10 @@ func (c *controller) createBlockVolume(ctx context.Context, req *csi.CreateVolum
 			return nil, csifault.CSIInternalFault, logger.LogNewErrorCodef(log, codes.Internal,
 				"failed to get shared datastores in kubernetes cluster. Error: %+v", err)
 		}
+		if len(sharedDatastores) == 0 {
+			return nil, csifault.CSIInternalFault, logger.LogNewErrorCode(log, codes.Internal,
+				"No datastore found for volume provisioning.")
+		}
 	}
 
 	if commonco.ContainerOrchestratorUtility.IsFSSEnabled(ctx, common.CSIAuthCheck) {
