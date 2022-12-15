kubernetes-sigs/vsphere-csi-driver

commit 5571cf55993de8f08c465c6120f588b7685a85de

mark volume detach as success when Node vm is deleted from vcenter

pkg/csi/service/vanilla/controller.go
@@ -1247,8 +1247,14 @@ func (c *controller) ControllerUnpublishVolume(ctx context.Context, req *csi.Con
 			node, err = c.nodeMgr.GetNodeByName(ctx, req.NodeId)
 		}
 		if err != nil {
-			return nil, csifault.CSIInternalFault, logger.LogNewErrorCodef(log, codes.Internal,
-				"failed to find VirtualMachine for node:%q. Error: %v", req.NodeId, err)
+			if err == cnsvsphere.ErrVMNotFound {
+				log.Infof("Virtual Machine for Node ID: %v is not present in the VC Inventory. "+
+					"Marking ControllerUnpublishVolume for Volume: %q as successful.", req.NodeId, req.VolumeId)
+				return &csi.ControllerUnpublishVolumeResponse{}, "", nil
+			} else {
+				return nil, csifault.CSIInternalFault, logger.LogNewErrorCodef(log, codes.Internal,
+					"failed to find VirtualMachine for node:%q. Error: %v", req.NodeId, err)
+			}
 		}
 		faultType, err = common.DetachVolumeUtil(ctx, c.manager, node, req.VolumeId)
 		if err != nil {

commit cca6a05cf12cfed63a1e3526b13edaee912cc1ee

Fixed wrong error checking in ControllerUnpublishVolume
A comparison with cnsvsphere.ErrVMNotFound will not work since NodeManager returns its own error ErrNodeNotFound

The error was introduced by 5571cf55993de8f08c465c6120f588b7685a85de

pkg/csi/service/vanilla/controller.go
@@ -2236,7 +2236,7 @@ func (c *controller) ControllerUnpublishVolume(ctx context.Context, req *csi.Con
 			nodevm, err = c.nodeMgr.GetNodeByName(ctx, req.NodeId)
 		}
 		if err != nil {
-			if err == cnsvsphere.ErrVMNotFound {
+			if err == node.ErrNodeNotFound {
 				log.Infof("Virtual Machine for Node ID: %v is not present in the VC Inventory. "+
 					"Marking ControllerUnpublishVolume for Volume: %q as successful.", req.NodeId, req.VolumeId)
 				return &csi.ControllerUnpublishVolumeResponse{}, "", nil