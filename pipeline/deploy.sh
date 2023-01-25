2a4aad37204c6796033d3a4411ed523340521b2c
Sandeep Pissay Srinivasa Rao <ssrinivas@vmware.com>
2023-01-25 12:59:01 -0800

    Addressed review comments.

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -15,16 +15,14 @@ then
 fi
 
 # Borrow a testbed from CSI Testbed Pool Svc.
-testbed=`curl -X 'GET' ${CNS_TESTBEDPOOL_SVC_URL}`
-if [ $? -ne 0 ]
+if ! testbed=$(curl -X 'GET' "${CNS_TESTBEDPOOL_SVC_URL}");
 then
 	echo "Unable to borrow a testbed!"
 	exit 1
 fi
 echo "TestbedInfo: $testbed"
 
-vcIp=`echo $testbed | jq '.vcIp'|tr -d '"'`
-if [ $? -ne 0 ]
+if ! vcIp=$(echo "$testbed" | jq '.vcIp'|tr -d '"');
 then
 	echo "Error getting the vcIp!"
 	exit 1
@@ -32,19 +30,17 @@ fi
 
 echo "VC_IP=$vcIp" >> build.env
 
-svAdminCreds=`echo $testbed | jq '.svAdminCreds'|tr -d '"'|base64 -d`
-if [ $? -ne 0 ]
+if ! svAdminCreds=$(echo "$testbed" | jq '.svAdminCreds'|tr -d '"'|base64 -d);
 then
 	echo "Error getting the svAdminCreds!"
 	exit 1
 fi
 echo "svAdminCreds = $svAdminCreds"
 SV_KUBECONFIG=/tmp/$$
-echo $testbed | jq '.svAdminCreds'|tr -d '"'|base64 -d > $SV_KUBECONFIG
+echo "$testbed" | jq '.svAdminCreds'|tr -d '"'|base64 -d > $SV_KUBECONFIG
 export KUBECONFIG=$SV_KUBECONFIG
 
-kustomize build pipeline/dev | envsubst | kubectl apply -f -
-if [ $? -ne 0 ]
+if ! kustomize build pipeline/dev | envsubst | kubectl apply -f -;
 then
 	echo "Error patching the CSI images in the testbed!"
 	exit 1
@@ -55,19 +51,10 @@ echo "Sleeping for 60 seconds..."
 sleep 60
 
 # Wait for 2 mins for the CSI deployment to be ready.
-kubectl wait deployment -n vmware-system-csi vsphere-csi-controller --for=jsonpath="{.status.readyReplicas}"=3 --timeout=120s
-if [ $? -ne 0 ]
+if ! kubectl wait deployment -n vmware-system-csi vsphere-csi-controller --for=jsonpath="{.status.readyReplicas}"=3 --timeout=120s;
 then
 	echo "CSI deployment is not ready within 120s."
 	exit 1
 fi
-
-# Wait for 2 mins for the CSI webhook to be ready.
-#kubectl wait deployment -n vmware-system-csi vsphere-csi-webhook --for=jsonpath="{.status.readyReplicas}"=3 --timeout=120s
-#if [ $? -ne 0 ]
-#then
-#	echo "CSI webhook is not ready within 120s."
-#	exit 1
-#fi
 
 echo "Successfully patched the CSI images."
