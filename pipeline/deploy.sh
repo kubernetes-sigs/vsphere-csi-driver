e89885b99f1d049c185178834b98976e8b5d5e3f
rpanduranga <rpanduranga@vmware.com>
2023-03-07 22:15:40 +0530

    [CICD-E2E-Test-Stage] Adding e2e-test stage into CICD Pipeline

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -1,48 +1,124 @@
 #!/bin/bash
 
+# Copyright 2023 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set +x
 
 if [[ -z "${VSPHERE_CSI_CONTROLLER_IMAGE}" ]]
 then
-	echo "Env variable unset: VSPHERE_CSI_CONTROLLER_IMAGE"
+	echo "Env variable unset: VSPHERE_CSI_CONTROLLER_IMAGE."
 	exit 1
 fi
 
 if [[ -z "${VSPHERE_SYNCER_IMAGE}" ]]
 then
-	echo "Env variable unset: VSPHERE_SYNCER_IMAGE"
+	echo "Env variable unset: VSPHERE_SYNCER_IMAGE."
 	exit 1
 fi
 
 # Borrow a testbed from CSI Testbed Pool Svc.
-if ! testbed=$(curl -X 'GET' "${CNS_TESTBEDPOOL_SVC_URL}");
+if ! testbed=$(curl -X 'PUT' "${CNS_TESTBEDPOOL_SVC_URL}" -H 'accept: application/json' -u "${CNS_MANAGER_USERNAME}:${CNS_MANAGER_PASSWORD}");
 then
-	echo "Unable to borrow a testbed!"
+	echo "Unable to borrow a testbed."
 	exit 1
 fi
-echo "TestbedInfo: $testbed"
 
+echo "TestbedInfo: $testbed ."
+
+# Extract testbed ID from borrow testbed API Response.
+if ! id=$(echo "$testbed" | jq '.id'|tr -d '"');
+then
+	echo "Error getting the id."
+	exit 1
+fi
+
+# Store the testbed ID in a local file on the workspace.
+echo "$id" > pipeline/placeholder.id
+
+# Extract VC IP from borrow testbed API Response.
 if ! vcIp=$(echo "$testbed" | jq '.vcIp'|tr -d '"');
 then
-	echo "Error getting the vcIp!"
+	echo "Error getting the vcIp."
 	exit 1
 fi
 
-echo "VC_IP=$vcIp" >> build.env
-
-if ! svAdminCreds=$(echo "$testbed" | jq '.svAdminCreds'|tr -d '"'|base64 -d);
+# Extract External Gateway VM IP from borrow testbed API Response.
+if ! externalVMGatewayIp=$(echo "$testbed" | jq '.externalVMGatewayIp'|tr -d '"');
 then
-	echo "Error getting the svAdminCreds!"
+	echo "Error getting the externalVMGatewayIp."
 	exit 1
 fi
-echo "svAdminCreds = $svAdminCreds"
+
+
+# Extract VC root password from borrow testbed API Response.
+if ! vcRootPassword=$(echo "$testbed" | jq '.vcRootPassword'|tr -d '"');
+then
+	echo "Error getting the vcRootPassword."
+	exit 1
+fi
+
+# Extract VC username from borrow testbed API Response.
+if ! vimUsername=$(echo "$testbed" | jq '.vimUsername'|tr -d '"');
+then
+	echo "Error getting the vimUsername."
+	exit 1
+fi
+
+# Extract VC Admin password from borrow testbed API Response.
+if ! vimPassword=$(echo "$testbed" | jq '.vimPassword'|tr -d '"');
+then
+	echo "Error getting the vimPassword."
+	exit 1
+fi
+
+# Print all the values into Console.
+echo "VSPHERE_CSI_CONTROLLER_IMAGE = $VSPHERE_CSI_CONTROLLER_IMAGE"
+echo "VSPHERE_SYNCER_IMAGE = $VSPHERE_SYNCER_IMAGE"
+
+# Store all the values into Artifacts.
+{ echo "id=$id"; echo "vcIp=$vcIp"; echo "vcRootPassword=$vcRootPassword"; echo "vimPassword=$vimPassword"; echo "vimUsername=$vimUsername"; echo "externalVMGatewayIp=$externalVMGatewayIp";} >> ./env.json
+# echo "vcIp=$vcIp" >> ./env.json
+# echo "vcRootPassword=$vcRootPassword" >> ./env.json
+# echo "vimPassword=$vimPassword" >> ./env.json
+# echo "vimUsername=$vimUsername" >> ./env.json
+# echo "externalVMGatewayIp=$externalVMGatewayIp" >> ./env.json
+
 SV_KUBECONFIG=/tmp/$$
-echo "$testbed" | jq '.svAdminCreds'|tr -d '"'|base64 -d > $SV_KUBECONFIG
+
+echo "$testbed" | jq '.kubeConfig'|tr -d '"'|base64 -d > $SV_KUBECONFIG
+
 export KUBECONFIG=$SV_KUBECONFIG
 
+# Pod status on testbed before patching the CSI Images
+kubectl get pods -n vmware-system-csi
+
+K8S_MAJOR_VERSION=$(kubectl version -o json | jq .serverVersion.major | tr -d '"')
+K8S_MINOR_VERSION=$(kubectl version -o json | jq .serverVersion.minor | tr -d '"')
+
+echo "K8S_MAJOR_VERSION=$K8S_MAJOR_VERSION" >> build.env
+echo "K8S_MINOR_VERSION=$K8S_MINOR_VERSION" >> build.env
+
+# Replace the kubernetes version in kustomization.yaml
+sed -i "s#- ../../manifests/supervisorcluster/1.22#- ../../manifests/supervisorcluster/$K8S_MAJOR_VERSION.$K8S_MINOR_VERSION#g" pipeline/dev/kustomization.yaml
+
+echo "cat pipeline/dev/kustomization.yaml"
+cat pipeline/dev/kustomization.yaml
+ 
 if ! kustomize build pipeline/dev | envsubst | kubectl apply -f -;
 then
-	echo "Error patching the CSI images in the testbed!"
+	echo "Error patching the CSI images in the testbed."
 	exit 1
 fi
 
@@ -56,5 +132,8 @@ then
 	echo "CSI deployment is not ready within 120s."
 	exit 1
 fi
+
+# Pod status on testbed after patching the CSI Images
+kubectl get pods -n vmware-system-csi
 
 echo "Successfully patched the CSI images."
--- pipeline/e2e-tests.sh
+++ pipeline/e2e-tests.sh
@@ -1,8 +1,104 @@
 #!/bin/bash
 
-set -x
+# Copyright 2023 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-echo "Running e2e tests..."
-# TODO: Implement the script to run the e2e tests.
+set +x
 
-echo "Completed running e2e tests."
+id=$(grep id ./env.json | cut -d = -f2)
+# Store the testbed ID in a local file on the workspace.
+echo "$id" > pipeline/placeholder.id
+
+vcIp=$(grep vcIp ./env.json | cut -d = -f2)
+vimUsername=$(grep vimUsername ./env.json | cut -d = -f2)
+vimPassword=$(grep vimPassword ./env.json | cut -d = -f2)
+vcRootPassword=$(grep vcRootPassword ./env.json | cut -d = -f2)
+
+echo "$vcIp"
+echo "$vcRootPassword" > vc_pwd
+
+export GOVC_INSECURE=1
+export GOVC_URL="https://$vimUsername:$vimPassword@$vcIp"
+
+DATACENTER=$(govc datacenter.info | grep -i path | awk '{print $2}')
+export DATACENTER=$DATACENTER
+
+COMPUTE_CLUSTER_NAME=$(govc namespace.cluster.ls | awk -F'/' '{print $5}')
+export COMPUTE_CLUSTER_NAME=$COMPUTE_CLUSTER_NAME
+
+export E2E_TEST_CONF_FILE=$CI_BUILDS_DIR/e2eTest.conf
+
+echo "$GOVC_URL"
+echo "$DATACENTER"
+echo "$COMPUTE_CLUSTER_NAME"
+echo "$E2E_TEST_CONF_FILE"
+
+tee "$E2E_TEST_CONF_FILE" >/dev/null <<EOF
+[Global]
+insecure-flag = "true"
+hostname = "$vcIp"
+user = "$vimUsername"
+password = "$vimPassword"
+port = "443"
+datacenters = "$DATACENTER"
+EOF
+
+mkdir -p ~/.kube
+ssh-keygen -R "$vcIp"
+sshpass -f vc_pwd ssh root@"$vcIp" -o "StrictHostKeyChecking no" "/usr/lib/vmware-wcp/decryptK8Pwd.py" > master.txt
+K8S_MASTER_IP=$(awk 'FNR == 6 {print $2}' master.txt)
+K8S_MASTER_TKN=$(awk 'FNR == 7 {print $2}' master.txt)
+export KUBECONFIG=~/.kube/config
+ssh-keygen -R "$K8S_MASTER_IP"
+echo "$K8S_MASTER_TKN" > token
+sshpass -f token scp -o "StrictHostKeyChecking no" root@"$K8S_MASTER_IP":~/.kube/config $KUBECONFIG
+export master_ip="server: https://$K8S_MASTER_IP:6443"
+sed -i "/server: https:\\/\\/127.0.0.1/c\\ \\ \\ \\ ${master_ip}" $KUBECONFIG
+
+echo "*******"
+govc datacenter.info | grep -i path | awk '{print $2}'
+govc namespace.cluster.ls | awk -F'/' '{print $5}'
+echo "*******"
+
+echo $KUBECONFIG
+export E2E_TEST_CONF_FILE="$E2E_TEST_CONF_FILE"
+export VC_REBOOT_WAIT_TIME=1020
+export USER=root
+export VOLUME_OPS_SCALE=5
+export CLUSTER_FLAVOR="WORKLOAD"
+export STORAGE_POLICY_FOR_SHARED_DATASTORES="shared-ds-policy"
+export STORAGE_POLICY_FOR_SHARED_DATASTORES_2="shared-ds-policy-2"
+# need to create tagged vm storage policy
+export STORAGE_POLICY_FOR_NONSHARED_DATASTORES="non-shared-ds-policy"
+export STORAGE_POLICY_WITH_THICK_PROVISIONING=thick-ds-policy
+SHARED_VSPHERE_DATASTORE_URL=$(govc datastore.info "$DATACENTER"/datastore/vsanDatastore | grep URL | awk '{print $2}')
+[ -z "$SHARED_VSPHERE_DATASTORE_URL" ] && SHARED_VSPHERE_DATASTORE_URL=$(govc datastore.info "$DATACENTER"/datastore/nfs* | grep URL | awk '{print $2}')
+export SHARED_VSPHERE_DATASTORE_URL
+LIST_NONSHARED_DATASTORE_URL=$(govc datastore.info "$DATACENTER"/datastore/\*local-\* | grep URL | awk '{print $2}')
+[ -z "$LIST_NONSHARED_DATASTORE_URL" ] && LIST_NONSHARED_DATASTORE_URL=$(govc datastore.info "$DATACENTER"/datastore/\*datastore\* | grep URL | awk '{print $2}')
+export LIST_NONSHARED_DATASTORE_URL
+export FULL_SYNC_WAIT_TIME=350
+export SVC_NAMESPACE="e2e-test-namespace"
+export SVC_NAMESPACE_TO_DELETE="e2e-namespace-to-delete"
+VM_NAME=$(govc ls "$DATACENTER"/vm | grep vmdktestvm | awk '{ gsub("/vm/"," ");print $2}')
+export VM_NAME
+export DISK_URL_PATH="https://$vcIp/folder/$VM_NAME/$VM_NAME""_1.vmdk?dcPath=$DATACENTER&dsName=vsanDatastore"
+GOPATH=$(go env GOPATH)
+export PATH=$PATH:$GOPATH/bin
+export GINKGO_OPTS="--no-color --always-emit-ginkgo-writer --progress --trace -p"
+echo "GINKGO_FOCUS=${CNS_CSI_GINKGO_FOCUS}"
+export GINKGO_FOCUS="${CNS_CSI_GINKGO_FOCUS}"
+
+echo "Running E2E tests."
+make test-e2e
