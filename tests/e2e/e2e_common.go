546ddc92dbf90de2cee91f710395f93e049e8624
Hemant Kumar <gnufied@users.noreply.github.com>
2023-06-01 13:47:46 -0400

    Allow topology to be configured day 2 operation (#2362)
    * Allow topology to be configured as day2 operation
    
    Only allow topology to be configured as day2 operation if
    it was not configured before.
    
    * Fix syncer code to update topology
    
    Enable update events
    
    * Move json-patch as an explicit dependency
    
    * Only reset topology status on vanilla clusters
    
    * Rename topologyFound to csiNodeTopologyFound

--- tests/e2e/e2e_common.go
+++ tests/e2e/e2e_common.go
@@ -127,7 +127,7 @@ const (
 	kubeAPIRecoveryTime                       = 1 * time.Minute
 	kubeSystemNamespace                       = "kube-system"
 	kubeletConfigYaml                         = "/var/lib/kubelet/config.yaml"
-	nfs4FSType                                = "nfs4"
+	nfs4FSType                                = "nfs3"
 	objOrItemNotFoundErr                      = "The object or item referred to could not be found"
 	passorwdFilePath                          = "/etc/vmware/wcp/.storageUser"
 	podContainerCreatingState                 = "ContainerCreating"
