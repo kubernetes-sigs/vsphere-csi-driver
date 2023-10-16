7a1bdd8768ceeac305ca42efe20f08cff981b0bd
Chethan Venkatesh <chethanv@vmware.com>
2023-10-16 10:29:01 -0700

    Rename interface method names and prevent node cache update during attach/detach (#2602)

--- pkg/csi/service/common/commonco/k8sorchestrator/topology.go
+++ pkg/csi/service/common/commonco/k8sorchestrator/topology.go
@@ -1113,10 +1113,10 @@ func (volTopology *controllerVolumeTopology) getTopologySegmentsWithMatchingNode
 			var nodeVM *cnsvsphere.VirtualMachine
 			if volTopology.isCSINodeIdFeatureEnabled &&
 				volTopology.clusterFlavor == cnstypes.CnsClusterFlavorVanilla {
-				nodeVM, err = volTopology.nodeMgr.GetNode(ctx,
+				nodeVM, err = volTopology.nodeMgr.GetNodeVMAndUpdateCache(ctx,
 					nodeTopologyInstance.Spec.NodeUUID, nil)
 			} else {
-				nodeVM, err = volTopology.nodeMgr.GetNodeByName(ctx,
+				nodeVM, err = volTopology.nodeMgr.GetNodeVMByNameAndUpdateCache(ctx,
 					nodeTopologyInstance.Spec.NodeID)
 			}
 			if err != nil {
@@ -1184,10 +1184,10 @@ func (volTopology *controllerVolumeTopology) getNodesMatchingTopologySegment(ctx
 			var nodeVM *cnsvsphere.VirtualMachine
 			if volTopology.isCSINodeIdFeatureEnabled &&
 				volTopology.clusterFlavor == cnstypes.CnsClusterFlavorVanilla {
-				nodeVM, err = volTopology.nodeMgr.GetNode(ctx,
+				nodeVM, err = volTopology.nodeMgr.GetNodeVMAndUpdateCache(ctx,
 					nodeTopologyInstance.Spec.NodeUUID, nil)
 			} else {
-				nodeVM, err = volTopology.nodeMgr.GetNodeByName(ctx,
+				nodeVM, err = volTopology.nodeMgr.GetNodeVMByNameAndUpdateCache(ctx,
 					nodeTopologyInstance.Spec.NodeID)
 			}
 			if err != nil {
