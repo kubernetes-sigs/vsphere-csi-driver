88b49ddb799bd3b9a55321f689ad3b650c832f93
Liping Xue <lipingx@vmware.com>
2023-12-08 13:23:18 -0800

    Add more logs when creating CnsRegisterVolume CRD.

--- pkg/syncer/cnsoperator/manager/init.go
+++ pkg/syncer/cnsoperator/manager/init.go
@@ -149,15 +149,20 @@ func InitCnsOperator(ctx context.Context, clusterFlavor cnstypes.CnsClusterFlavo
 				stretchedSupervisor = true
 			}
 		}
+		if stretchedSupervisor {
+			log.Info("Observed stretchedSupervisor setup")
+		}
 		if !stretchedSupervisor ||
 			(stretchedSupervisor && commonco.ContainerOrchestratorUtility.IsFSSEnabled(ctx, common.PodVMOnStretchedSupervisor)) {
 			// Create CnsRegisterVolume CRD from manifest.
+			log.Infof("Creating %q CRD", cnsoperatorv1alpha1.CnsRegisterVolumePlural)
 			err = k8s.CreateCustomResourceDefinitionFromManifest(ctx, cnsoperatorconfig.EmbedCnsRegisterVolumeCRFile,
 				cnsoperatorconfig.EmbedCnsRegisterVolumeCRFileName)
 			if err != nil {
 				log.Errorf("Failed to create %q CRD. Err: %+v", cnsoperatorv1alpha1.CnsRegisterVolumePlural, err)
 				return err
 			}
+			log.Infof("%q CRD is created successfully", cnsoperatorv1alpha1.CnsRegisterVolumePlural)
 		}
 
 		if !stretchedSupervisor {
@@ -182,8 +187,10 @@ func InitCnsOperator(ctx context.Context, clusterFlavor cnstypes.CnsClusterFlavo
 			}
 		}
 
-		if !stretchedSupervisor {
+		if !stretchedSupervisor ||
+			(stretchedSupervisor && commonco.ContainerOrchestratorUtility.IsFSSEnabled(ctx, common.PodVMOnStretchedSupervisor)) {
 			// Clean up routine to cleanup successful CnsRegisterVolume instances.
+			log.Info("Starting go routine to cleanup successful CnsRegisterVolume instances.")
 			err = watcher(ctx, cnsOperator)
 			if err != nil {
 				log.Error("Failed to watch on config file for changes to CnsRegisterVolumesCleanupIntervalInMin. Error: %+v",
