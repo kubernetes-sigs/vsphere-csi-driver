270fea66752e56c5f6f7d0af6d2cdcf23e7aa453
Chethan Venkatesh <chethanv@vmware.com>
2024-05-07 11:24:31 -0700

    Bump up sidecar versions to latest releases (#2876)

--- manifests/vanilla/deploy-csi-snapshot-components.sh
+++ manifests/vanilla/deploy-csi-snapshot-components.sh
@@ -50,7 +50,7 @@ if ! command -v kubectl > /dev/null; then
   exit 1
 fi
 
-qualified_version="v7.0.1"
+qualified_version="v7.0.2"
 volumesnapshotclasses_crd="volumesnapshotclasses.snapshot.storage.k8s.io"
 volumesnapshotcontents_crd="volumesnapshotcontents.snapshot.storage.k8s.io"
 volumesnapshots_crd="volumesnapshots.snapshot.storage.k8s.io"
