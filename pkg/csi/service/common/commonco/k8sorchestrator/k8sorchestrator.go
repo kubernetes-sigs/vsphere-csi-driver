26f23dde96960fa138eeddaed6298db670dc067f
John Cho <john.cho@broadcom.com>
2025-07-05 02:07:25 -0700

    Remove FSS max-pvscsi-targets-per-vm

--- pkg/csi/service/common/commonco/k8sorchestrator/k8sorchestrator.go
+++ pkg/csi/service/common/commonco/k8sorchestrator/k8sorchestrator.go
@@ -361,7 +361,6 @@ func getReleasedVanillaFSS() map[string]struct{} {
 		common.ListVolumes:                    {},
 		common.CnsMgrSuspendCreateVolume:      {},
 		common.TopologyPreferentialDatastores: {},
-		common.MaxPVSCSITargetsPerVM:          {},
 		common.MultiVCenterCSITopology:        {},
 		common.CSIInternalGeneratedClusterID:  {},
 		common.TopologyAwareFileVolume:        {},
