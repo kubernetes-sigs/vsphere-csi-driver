fff2b835f2986b3ca8030ed4f9823b1e6ef45da7
skogta <36151582+skogta@users.noreply.github.com>
2025-08-01 06:13:14 +0530

    For NSX setup, it is possible that VirtualNetwork is not defined for a given VM service VM. (#3446)
    In that case, CSI should rely on the NamespaceNetworkInfo CR's SNAT IP to determine the VM's external IP.

---
+++ pkg/syncer/cnsoperator/util/util_test.go
@@ -0,0 +1,135 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic/fake"
+)
+
+func TestGetSnatIpFromNamespaceNetworkInfo(t *testing.T) {
+	ctx := context.Background()
+	gvr := schema.GroupVersionResource{
+		Group:    "nsx.vmware.com",
+		Version:  "v1alpha1",
+		Resource: "namespacenetworkinfos",
+	}
+
+	namespace := "test-namespace"
+	vmName := "test-vm"
+
+	tests := []struct {
+		name           string
+		initialObjects []runtime.Object
+		expectedIP     string
+		expectError    bool
+	}{
+		{
+			name: "Happy path - SNAT IP present",
+			initialObjects: []runtime.Object{
+				&unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "nsx.vmware.com/v1alpha1",
+						"kind":       "NamespaceNetworkInfo",
+						"metadata": map[string]interface{}{
+							"name":      namespace,
+							"namespace": namespace,
+						},
+						"topology": map[string]interface{}{
+							"defaultEgressIP": "10.10.10.10",
+						},
+					},
+				},
+			},
+			expectedIP:  "10.10.10.10",
+			expectError: false,
+		},
+		{
+			name:           "Error - resource not found",
+			initialObjects: []runtime.Object{}, // Nothing in fake client
+			expectedIP:     "",
+			expectError:    true,
+		},
+		{
+			name: "Error - defaultEgressIP missing",
+			initialObjects: []runtime.Object{
+				&unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "nsx.vmware.com/v1alpha1",
+						"kind":       "NamespaceNetworkInfo",
+						"metadata": map[string]interface{}{
+							"name":      namespace,
+							"namespace": namespace,
+						},
+						"spec": map[string]interface{}{
+							"topology": map[string]interface{}{
+								// defaultEgressIP intentionally missing
+							},
+						},
+					},
+				},
+			},
+			expectedIP:  "",
+			expectError: true,
+		},
+		{
+			name: "Error - defaultEgressIP is empty",
+			initialObjects: []runtime.Object{
+				&unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "nsx.vmware.com/v1alpha1",
+						"kind":       "NamespaceNetworkInfo",
+						"metadata": map[string]interface{}{
+							"name":      namespace,
+							"namespace": namespace,
+						},
+						"spec": map[string]interface{}{
+							"topology": map[string]interface{}{
+								"defaultEgressIP": "",
+							},
+						},
+					},
+				},
+			},
+			expectedIP:  "",
+			expectError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			scheme := runtime.NewScheme()
+			scheme.AddKnownTypes(gvr.GroupVersion())
+			fakeClient := fake.NewSimpleDynamicClient(scheme, tt.initialObjects...)
+			ip, err := getSnatIpFromNamespaceNetworkInfo(ctx, fakeClient, namespace, vmName)
+
+			if tt.expectError {
+				assert.Error(t, err)
+				assert.Empty(t, ip)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expectedIP, ip)
+			}
+		})
+	}
+}
