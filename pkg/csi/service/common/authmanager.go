16c72e1383ff85399b3c553c2c56dcae669be61b
Chethan Venkatesh <chethanv@vmware.com>
2025-09-05 22:35:24 -0700

    Revert session manager commits (#3533)
    * Revert "Implement session manager client on CSI (#3419)"
    
    This reverts commit 0ec9586e695dd181149421152f3d2b50e13232c3.
    
    * Revert "Refactor tag manager rest client (#3412)"
    
    This reverts commit 50412088094ef5b3fdad919c8464c3f9021b6c11.
    
    * Resove conflicts to remove unsed methods in topology package
    
    * Resolve conflicts in test utils

--- pkg/csi/service/common/authmanager.go
+++ pkg/csi/service/common/authmanager.go
@@ -337,11 +337,7 @@ func getDatastoresWithBlockVolumePrivs(ctx context.Context, vc *cnsvsphere.Virtu
 	authMgr := object.NewAuthorizationManager(vc.Client.Client)
 	privIds := []string{DsPriv, SysReadPriv}
 
-	userName, err := vc.GetActiveUser(ctx)
-	if err != nil {
-		return nil, err
-	}
-
+	userName := vc.Config.Username
 	// Invoke authMgr function HasUserPrivilegeOnEntities.
 	result, err := authMgr.HasUserPrivilegeOnEntities(ctx, entities, userName, privIds) // entities empty -> error
 	if err != nil {
@@ -444,14 +440,10 @@ func getFSEnabledClustersWithPriv(ctx context.Context, vc *cnsvsphere.VirtualCen
 		clusterComputeResources = append(clusterComputeResources, clusterComputeResource...)
 	}
 
-	userName, err := vc.GetActiveUser(ctx)
-	if err != nil {
-		return nil, err
-	}
-
 	// Get Clusters with HostConfigStoragePriv.
 	authMgr := object.NewAuthorizationManager(vc.Client.Client)
 	privIds := []string{HostConfigStoragePriv}
+	userName := vc.Config.Username
 	var entities []vim25types.ManagedObjectReference
 	clusterComputeResourcesMap := make(map[string]*object.ClusterComputeResource)
 	for _, cluster := range clusterComputeResources {
--- pkg/syncer/metadatasyncer.go
+++ pkg/syncer/metadatasyncer.go
@@ -2328,12 +2328,9 @@ func ReloadConfiguration(metadataSyncer *metadataSyncInformer, reconnectToVCFrom
 		if newVCConfig != nil {
 			var vcenter *cnsvsphere.VirtualCenter
 			newVCConfig.ReloadVCConfigForNewClient = true
-			vcConfig := metadataSyncer.configInfo.Cfg.VirtualCenter[metadataSyncer.host]
 			if metadataSyncer.host != newVCConfig.Host ||
-				vcConfig.User != newVCConfig.Username ||
-				vcConfig.Password != newVCConfig.Password ||
-				vcConfig.VCSessionManagerURL != newVCConfig.VCSessionManagerURL ||
-				vcConfig.VCSessionManagerToken != newVCConfig.VCSessionManagerToken ||
+				metadataSyncer.configInfo.Cfg.VirtualCenter[metadataSyncer.host].User != newVCConfig.Username ||
+				metadataSyncer.configInfo.Cfg.VirtualCenter[metadataSyncer.host].Password != newVCConfig.Password ||
 				reconnectToVCFromNewConfig {
 				// Verify if new configuration has valid credentials by connecting
 				// to vCenter. Proceed only if the connection succeeds, else return
