a200475ca3a048656bc08678187478cbf09e0361
sipriyaa <93115138+sipriyaa@users.noreply.github.com>
2025-10-06 13:28:58 +0530

    Test fixes for EF UTS production for tkg (#3650)

--- tests/e2e/vsphere_volume_expansion.go
+++ tests/e2e/vsphere_volume_expansion.go
@@ -3133,9 +3133,12 @@ func invokeTestForVolumeExpansion(f *framework.Framework, client clientset.Inter
 		verifyPVSizeinSupervisor(svcPVCName, newSize)
 	}
 
-	ginkgo.By("Checking for conditions on pvc")
-	pvclaim, err = waitForPVCToReachFileSystemResizePendingCondition(client, namespace, pvclaim.Name, pollTimeout)
-	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	isPrivateNetwork := GetBoolEnvVarOrDefault("IS_PRIVATE_NETWORK", false)
+	if !isPrivateNetwork {
+		ginkgo.By("Checking for conditions on pvc")
+		pvclaim, err = waitForPVCToReachFileSystemResizePendingCondition(client, namespace, pvclaim.Name, pollTimeout)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	}
 
 	if guestCluster {
 		ginkgo.By("Checking for 'FileSystemResizePending' status condition on SVC PVC")
