8777b54d8f3ca0b66d8a2769068cbe55cd899cbd
bn023052 <bhavya.nayak@broadcom.com>
2025-10-07 23:51:17 +0530

    Helpers for linked-clone

--- tests/e2e/helpers/util.go
+++ tests/e2e/helpers/util.go
@@ -1,6 +1,26 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package helpers
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+
+	"k8s.io/kubernetes/test/e2e/framework"
+)
 
 // Helper function to check if a string exists in a slice
 func ContainsItem(slice []string, item string) bool {
@@ -21,4 +41,17 @@ func IsValuePresentInTheList(strArr []string, str string) bool {
 		}
 	}
 	return false
+}
+
+// This method is used to remove the unit and convert string to int
+func ParseMi(value string) (int, error) {
+	var numeric string
+	// Remove the "Mi" or "Gi" suffix and convert to int
+	if strings.HasSuffix(value, "Mi") {
+		numeric = strings.TrimSuffix(value, "Mi")
+	} else if strings.HasSuffix(value, "Gi") {
+		numeric = strings.TrimSuffix(value, "Gi")
+	}
+	framework.Logf("Converting %s to int", numeric)
+	return strconv.Atoi(numeric)
 }
