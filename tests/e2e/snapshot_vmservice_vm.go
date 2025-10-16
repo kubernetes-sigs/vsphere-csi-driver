1fcce80d0b7f01cbf4719dd4ad8d4d19274b8ac0
kavyashree-r <71521316+kavyashree-r@users.noreply.github.com>
2025-10-17 00:32:41 +0530

    ef-f-stretched-svc label on strtech svc vmservice test (#3687)

--- tests/e2e/snapshot_vmservice_vm.go
+++ tests/e2e/snapshot_vmservice_vm.go
@@ -3057,8 +3057,8 @@ var _ bool = ginkgo.Describe("[snapshot-vmsvc] Snapshot VM Service VM", func() {
 	   25. Cleanup: Execute and verify the steps mentioned in the Delete snapshot mandatory checks
 	*/
 
-	ginkgo.It("[ef-stretched-svc][stretched-svc] Restoring snapshots and attaching it to a new vm service vms in a "+
-		"stretched supervisor env", ginkgo.Label(p0, block, stretchedSvc, snapshot, vc80), func() {
+	ginkgo.It("[ef-f-stretched-svc][stretched-svc] Restoring snapshots and attaching it to a new vm service vms in "+
+		"a stretched supervisor env", ginkgo.Label(p0, block, stretchedSvc, snapshot, vc80), func() {
 
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
