2342695fbf880d8516732729cf97b8ef89a87bb4
Deepak Kinni <deepak.kinni@broadcom.com>
2025-12-10 17:39:32 -0800

    Enhance storageclass validation for linkedclone volume creation (#3791)
    Signed-off-by: Deepak Kinni <deepak.kinni@broadcom.com>

--- pkg/syncer/admissionhandler/validatepvc_test.go
+++ pkg/syncer/admissionhandler/validatepvc_test.go
@@ -15,10 +15,12 @@ import (
 	"github.com/stretchr/testify/assert"
 	admissionv1 "k8s.io/api/admission/v1"
 	corev1 "k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientset "k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/vsphere-csi-driver/v3/pkg/csi/service/common"
 	k8s "sigs.k8s.io/vsphere-csi-driver/v3/pkg/kubernetes"
 )
 
@@ -619,6 +621,528 @@ func TestValidatePVC(t *testing.T) {
 
 			actualResponse := validatePVC(ctx, test.admissionReview.Request)
 			assert.Equal(t, test.expectedResponse, actualResponse)
+		})
+	}
+}
+
+func TestValidateGuestPVCOperation_LinkedClone_StorageClass(t *testing.T) {
+	// Store the original feature gate value and restore it after the test
+	originalFeatureGate := featureIsLinkedCloneSupportEnabled
+	defer func() {
+		featureIsLinkedCloneSupportEnabled = originalFeatureGate
+	}()
+	featureIsLinkedCloneSupportEnabled = true
+
+	const (
+		testLinkedCloneNamespace     = "test-ns"
+		testLinkedClonePVCName       = "test-lc-pvc"
+		testSourcePVCName            = "test-source-pvc"
+		testSnapshotName             = "test-snapshot"
+		testStorageClassA            = "storage-class-a"
+		testStorageClassB            = "storage-class-b"
+		testSvStorageClass1          = "wcpglobal-storage-profile"
+		testSvStorageClass2          = "wcpglobal-storage-profile-2"
+		testLinkedCloneSnapshotClass = "test-snapshot-class"
+	)
+
+	stringPtr := func(s string) *string {
+		return &s
+	}
+
+	boolPtr := func(b bool) *bool {
+		return &b
+	}
+
+	tests := []struct {
+		name                   string
+		kubeObjs               []runtime.Object
+		snapshotObjs           []runtime.Object
+		pvc                    *corev1.PersistentVolumeClaim
+		expectedAllowed        bool
+		expectedMessageContain string
+	}{
+		{
+			name: "LinkedClone with matching svStorageClass should succeed",
+			kubeObjs: []runtime.Object{
+				// Source PVC StorageClass with svStorageClass1
+				&storagev1.StorageClass{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testStorageClassA,
+					},
+					Provisioner: "csi.vsphere.vmware.com",
+					Parameters: map[string]string{
+						common.AttributeSupervisorStorageClass: testSvStorageClass1,
+					},
+				},
+				// Source PVC
+				&corev1.PersistentVolumeClaim{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      testSourcePVCName,
+						Namespace: testLinkedCloneNamespace,
+					},
+					Spec: corev1.PersistentVolumeClaimSpec{
+						StorageClassName: stringPtr(testStorageClassA),
+						AccessModes: []corev1.PersistentVolumeAccessMode{
+							corev1.ReadWriteOnce,
+						},
+						Resources: corev1.VolumeResourceRequirements{
+							Requests: corev1.ResourceList{
+								corev1.ResourceStorage: resource.MustParse("5Gi"),
+							},
+						},
+					},
+				},
+			},
+			snapshotObjs: []runtime.Object{
+				// VolumeSnapshot
+				&snapshotv1.VolumeSnapshot{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      testSnapshotName,
+						Namespace: testLinkedCloneNamespace,
+					},
+					Spec: snapshotv1.VolumeSnapshotSpec{
+						Source: snapshotv1.VolumeSnapshotSource{
+							PersistentVolumeClaimName: stringPtr(testSourcePVCName),
+						},
+						VolumeSnapshotClassName: stringPtr(testLinkedCloneSnapshotClass),
+					},
+					Status: &snapshotv1.VolumeSnapshotStatus{
+						ReadyToUse: boolPtr(true),
+					},
+				},
+			},
+			pvc: &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      testLinkedClonePVCName,
+					Namespace: testLinkedCloneNamespace,
+					Annotations: map[string]string{
+						common.AnnKeyLinkedClone: "true",
+					},
+				},
+				Spec: corev1.PersistentVolumeClaimSpec{
+					StorageClassName: stringPtr(testStorageClassA), // Same storage class
+					AccessModes: []corev1.PersistentVolumeAccessMode{
+						corev1.ReadWriteOnce,
+					},
+					Resources: corev1.VolumeResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceStorage: resource.MustParse("5Gi"),
+						},
+					},
+					DataSource: &corev1.TypedLocalObjectReference{
+						APIGroup: stringPtr("snapshot.storage.k8s.io"),
+						Kind:     "VolumeSnapshot",
+						Name:     testSnapshotName,
+					},
+				},
+			},
+			expectedAllowed:        true,
+			expectedMessageContain: "",
+		},
+		{
+			name: "LinkedClone with different StorageClass but same svStorageClass should succeed",
+			kubeObjs: []runtime.Object{
+				// Source PVC StorageClass with svStorageClass1
+				&storagev1.StorageClass{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testStorageClassA,
+					},
+					Provisioner: "csi.vsphere.vmware.com",
+					Parameters: map[string]string{
+						common.AttributeSupervisorStorageClass: testSvStorageClass1,
+					},
+				},
+				// LinkedClone PVC StorageClass with same svStorageClass1 but different name
+				&storagev1.StorageClass{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testStorageClassB,
+					},
+					Provisioner: "csi.vsphere.vmware.com",
+					Parameters: map[string]string{
+						common.AttributeSupervisorStorageClass: testSvStorageClass1, // Same supervisor storage class
+					},
+				},
+				// Source PVC
+				&corev1.PersistentVolumeClaim{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      testSourcePVCName,
+						Namespace: testLinkedCloneNamespace,
+					},
+					Spec: corev1.PersistentVolumeClaimSpec{
+						StorageClassName: stringPtr(testStorageClassA),
+						AccessModes: []corev1.PersistentVolumeAccessMode{
+							corev1.ReadWriteOnce,
+						},
+						Resources: corev1.VolumeResourceRequirements{
+							Requests: corev1.ResourceList{
+								corev1.ResourceStorage: resource.MustParse("5Gi"),
+							},
+						},
+					},
+				},
+			},
+			snapshotObjs: []runtime.Object{
+				// VolumeSnapshot
+				&snapshotv1.VolumeSnapshot{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      testSnapshotName,
+						Namespace: testLinkedCloneNamespace,
+					},
+					Spec: snapshotv1.VolumeSnapshotSpec{
+						Source: snapshotv1.VolumeSnapshotSource{
+							PersistentVolumeClaimName: stringPtr(testSourcePVCName),
+						},
+						VolumeSnapshotClassName: stringPtr(testLinkedCloneSnapshotClass),
+					},
+					Status: &snapshotv1.VolumeSnapshotStatus{
+						ReadyToUse: boolPtr(true),
+					},
+				},
+			},
+			pvc: &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      testLinkedClonePVCName,
+					Namespace: testLinkedCloneNamespace,
+					Annotations: map[string]string{
+						common.AnnKeyLinkedClone: "true",
+					},
+				},
+				Spec: corev1.PersistentVolumeClaimSpec{
+					StorageClassName: stringPtr(testStorageClassB), // Different storage class
+					AccessModes: []corev1.PersistentVolumeAccessMode{
+						corev1.ReadWriteOnce,
+					},
+					Resources: corev1.VolumeResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceStorage: resource.MustParse("5Gi"),
+						},
+					},
+					DataSource: &corev1.TypedLocalObjectReference{
+						APIGroup: stringPtr("snapshot.storage.k8s.io"),
+						Kind:     "VolumeSnapshot",
+						Name:     testSnapshotName,
+					},
+				},
+			},
+			expectedAllowed:        true,
+			expectedMessageContain: "",
+		},
+		{
+			name: "LinkedClone with mismatched svStorageClass should fail",
+			kubeObjs: []runtime.Object{
+				// Source PVC StorageClass with svStorageClass1
+				&storagev1.StorageClass{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testStorageClassA,
+					},
+					Provisioner: "csi.vsphere.vmware.com",
+					Parameters: map[string]string{
+						common.AttributeSupervisorStorageClass: testSvStorageClass1,
+					},
+				},
+				// LinkedClone PVC StorageClass with different svStorageClass2
+				&storagev1.StorageClass{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testStorageClassB,
+					},
+					Provisioner: "csi.vsphere.vmware.com",
+					Parameters: map[string]string{
+						common.AttributeSupervisorStorageClass: testSvStorageClass2, // Different supervisor storage class
+					},
+				},
+				// Source PVC
+				&corev1.PersistentVolumeClaim{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      testSourcePVCName,
+						Namespace: testLinkedCloneNamespace,
+					},
+					Spec: corev1.PersistentVolumeClaimSpec{
+						StorageClassName: stringPtr(testStorageClassA),
+						AccessModes: []corev1.PersistentVolumeAccessMode{
+							corev1.ReadWriteOnce,
+						},
+						Resources: corev1.VolumeResourceRequirements{
+							Requests: corev1.ResourceList{
+								corev1.ResourceStorage: resource.MustParse("5Gi"),
+							},
+						},
+					},
+				},
+			},
+			snapshotObjs: []runtime.Object{
+				// VolumeSnapshot
+				&snapshotv1.VolumeSnapshot{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      testSnapshotName,
+						Namespace: testLinkedCloneNamespace,
+					},
+					Spec: snapshotv1.VolumeSnapshotSpec{
+						Source: snapshotv1.VolumeSnapshotSource{
+							PersistentVolumeClaimName: stringPtr(testSourcePVCName),
+						},
+						VolumeSnapshotClassName: stringPtr(testLinkedCloneSnapshotClass),
+					},
+					Status: &snapshotv1.VolumeSnapshotStatus{
+						ReadyToUse: boolPtr(true),
+					},
+				},
+			},
+			pvc: &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      testLinkedClonePVCName,
+					Namespace: testLinkedCloneNamespace,
+					Annotations: map[string]string{
+						common.AnnKeyLinkedClone: "true",
+					},
+				},
+				Spec: corev1.PersistentVolumeClaimSpec{
+					StorageClassName: stringPtr(testStorageClassB), // Different storage class with different policy
+					AccessModes: []corev1.PersistentVolumeAccessMode{
+						corev1.ReadWriteOnce,
+					},
+					Resources: corev1.VolumeResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceStorage: resource.MustParse("5Gi"),
+						},
+					},
+					DataSource: &corev1.TypedLocalObjectReference{
+						APIGroup: stringPtr("snapshot.storage.k8s.io"),
+						Kind:     "VolumeSnapshot",
+						Name:     testSnapshotName,
+					},
+				},
+			},
+			expectedAllowed:        false,
+			expectedMessageContain: "svStorageClass mismatch",
+		},
+		{
+			name: "LinkedClone with missing svStorageClass in source StorageClass should fail",
+			kubeObjs: []runtime.Object{
+				// Source PVC StorageClass without svStorageClass
+				&storagev1.StorageClass{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testStorageClassA,
+					},
+					Provisioner: "csi.vsphere.vmware.com",
+					Parameters:  map[string]string{
+						// Missing common.AttributeSupervisorStorageClass
+					},
+				},
+				// LinkedClone PVC StorageClass with svStorageClass
+				&storagev1.StorageClass{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testStorageClassB,
+					},
+					Provisioner: "csi.vsphere.vmware.com",
+					Parameters: map[string]string{
+						common.AttributeSupervisorStorageClass: testSvStorageClass1,
+					},
+				},
+				// Source PVC
+				&corev1.PersistentVolumeClaim{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      testSourcePVCName,
+						Namespace: testLinkedCloneNamespace,
+					},
+					Spec: corev1.PersistentVolumeClaimSpec{
+						StorageClassName: stringPtr(testStorageClassA),
+						AccessModes: []corev1.PersistentVolumeAccessMode{
+							corev1.ReadWriteOnce,
+						},
+						Resources: corev1.VolumeResourceRequirements{
+							Requests: corev1.ResourceList{
+								corev1.ResourceStorage: resource.MustParse("5Gi"),
+							},
+						},
+					},
+				},
+			},
+			snapshotObjs: []runtime.Object{
+				// VolumeSnapshot
+				&snapshotv1.VolumeSnapshot{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      testSnapshotName,
+						Namespace: testLinkedCloneNamespace,
+					},
+					Spec: snapshotv1.VolumeSnapshotSpec{
+						Source: snapshotv1.VolumeSnapshotSource{
+							PersistentVolumeClaimName: stringPtr(testSourcePVCName),
+						},
+						VolumeSnapshotClassName: stringPtr(testLinkedCloneSnapshotClass),
+					},
+					Status: &snapshotv1.VolumeSnapshotStatus{
+						ReadyToUse: boolPtr(true),
+					},
+				},
+			},
+			pvc: &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      testLinkedClonePVCName,
+					Namespace: testLinkedCloneNamespace,
+					Annotations: map[string]string{
+						common.AnnKeyLinkedClone: "true",
+					},
+				},
+				Spec: corev1.PersistentVolumeClaimSpec{
+					StorageClassName: stringPtr(testStorageClassB),
+					AccessModes: []corev1.PersistentVolumeAccessMode{
+						corev1.ReadWriteOnce,
+					},
+					Resources: corev1.VolumeResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceStorage: resource.MustParse("5Gi"),
+						},
+					},
+					DataSource: &corev1.TypedLocalObjectReference{
+						APIGroup: stringPtr("snapshot.storage.k8s.io"),
+						Kind:     "VolumeSnapshot",
+						Name:     testSnapshotName,
+					},
+				},
+			},
+			expectedAllowed:        false,
+			expectedMessageContain: "does not have svstorageclass parameter",
+		},
+		{
+			name: "LinkedClone with missing svStorageClass in LinkedClone StorageClass should fail",
+			kubeObjs: []runtime.Object{
+				// Source PVC StorageClass with svStorageClass
+				&storagev1.StorageClass{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testStorageClassA,
+					},
+					Provisioner: "csi.vsphere.vmware.com",
+					Parameters: map[string]string{
+						common.AttributeSupervisorStorageClass: testSvStorageClass1,
+					},
+				},
+				// LinkedClone PVC StorageClass without svStorageClass
+				&storagev1.StorageClass{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testStorageClassB,
+					},
+					Provisioner: "csi.vsphere.vmware.com",
+					Parameters:  map[string]string{
+						// Missing common.AttributeSupervisorStorageClass
+					},
+				},
+				// Source PVC
+				&corev1.PersistentVolumeClaim{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      testSourcePVCName,
+						Namespace: testLinkedCloneNamespace,
+					},
+					Spec: corev1.PersistentVolumeClaimSpec{
+						StorageClassName: stringPtr(testStorageClassA),
+						AccessModes: []corev1.PersistentVolumeAccessMode{
+							corev1.ReadWriteOnce,
+						},
+						Resources: corev1.VolumeResourceRequirements{
+							Requests: corev1.ResourceList{
+								corev1.ResourceStorage: resource.MustParse("5Gi"),
+							},
+						},
+					},
+				},
+			},
+			snapshotObjs: []runtime.Object{
+				// VolumeSnapshot
+				&snapshotv1.VolumeSnapshot{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      testSnapshotName,
+						Namespace: testLinkedCloneNamespace,
+					},
+					Spec: snapshotv1.VolumeSnapshotSpec{
+						Source: snapshotv1.VolumeSnapshotSource{
+							PersistentVolumeClaimName: stringPtr(testSourcePVCName),
+						},
+						VolumeSnapshotClassName: stringPtr(testLinkedCloneSnapshotClass),
+					},
+					Status: &snapshotv1.VolumeSnapshotStatus{
+						ReadyToUse: boolPtr(true),
+					},
+				},
+			},
+			pvc: &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      testLinkedClonePVCName,
+					Namespace: testLinkedCloneNamespace,
+					Annotations: map[string]string{
+						common.AnnKeyLinkedClone: "true",
+					},
+				},
+				Spec: corev1.PersistentVolumeClaimSpec{
+					StorageClassName: stringPtr(testStorageClassB),
+					AccessModes: []corev1.PersistentVolumeAccessMode{
+						corev1.ReadWriteOnce,
+					},
+					Resources: corev1.VolumeResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceStorage: resource.MustParse("5Gi"),
+						},
+					},
+					DataSource: &corev1.TypedLocalObjectReference{
+						APIGroup: stringPtr("snapshot.storage.k8s.io"),
+						Kind:     "VolumeSnapshot",
+						Name:     testSnapshotName,
+					},
+				},
+			},
+			expectedAllowed:        false,
+			expectedMessageContain: "does not have svstorageclass parameter",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Create fake clients
+			kubeClient := fake.NewSimpleClientset(test.kubeObjs...)
+			snapshotClient := snapshotclientfake.NewSimpleClientset(test.snapshotObjs...)
+
+			// Patch k8s client functions
+			patches := gomonkey.ApplyFunc(
+				k8s.NewClient, func(ctx context.Context) (clientset.Interface, error) {
+					return kubeClient, nil
+				})
+			defer patches.Reset()
+
+			patches = gomonkey.ApplyFunc(
+				k8s.NewSnapshotterClient, func(ctx context.Context) (snapshotterClientSet.Interface, error) {
+					return snapshotClient, nil
+				})
+			defer patches.Reset()
+
+			// Marshal the PVC to raw JSON
+			pvcBytes, err := json.Marshal(test.pvc)
+			assert.NoError(t, err)
+
+			// Create admission request
+			admissionReq := &admissionv1.AdmissionRequest{
+				Kind: metav1.GroupVersionKind{
+					Kind: "PersistentVolumeClaim",
+				},
+				Operation: admissionv1.Create,
+				Namespace: testLinkedCloneNamespace,
+				Name:      testLinkedClonePVCName,
+				Object: runtime.RawExtension{
+					Raw: pvcBytes,
+				},
+			}
+
+			// Call the validation function
+			ctx := context.Background()
+			response := validateGuestPVCOperation(ctx, admissionReq)
+
+			// Verify the response
+			assert.Equal(t, test.expectedAllowed, response.Allowed,
+				"Expected allowed=%v but got allowed=%v. Message: %v",
+				test.expectedAllowed, response.Allowed, response.Result)
+
+			if !test.expectedAllowed && test.expectedMessageContain != "" {
+				assert.Contains(t, response.Result.Message, test.expectedMessageContain,
+					"Expected error message to contain '%s' but got: %s",
+					test.expectedMessageContain, response.Result.Message)
+			}
 		})
 	}
 }
