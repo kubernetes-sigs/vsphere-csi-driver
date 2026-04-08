1d3c860b1cd3973a4abb6c45e7644d8ff026755e
Saloni Kogta <saloni.kogta@broadcom.com>
2026-04-08 13:24:57 +0530

    Add StoragePolicyInfo CRD

---
+++ pkg/apis/cnsoperator/storagepolicyinfo/v1alpha1/storagepolicyinfo_types.go
@@ -0,0 +1,69 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Topology describes topology accessibility for the storage policy within the namespace.
+type Topology struct {
+	// AccessibleZones lists zones where the policy is accessible for this namespace.
+	// +optional
+	AccessibleZones []string `json:"accessibleZones,omitempty"`
+
+	// Error describes a failure condition when resolving topology for this storage policy.
+	// +optional
+	Error string `json:"error,omitempty"`
+}
+
+// StoragePolicyInfoStatus defines the observed state of StoragePolicyInfo.
+// +k8s:openapi-gen=true
+type StoragePolicyInfoStatus struct {
+	// Topology contains observed topology for this storage policy in the namespace.
+	// +optional
+	Topology *Topology `json:"topology,omitempty"`
+
+	// Error describes a failure condition when observing or reconciling this resource.
+	// +optional
+	Error string `json:"error,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:scope=Namespaced,shortName=spi,path=storagepolicyinfoes
+
+// StoragePolicyInfo is the Schema for the storagepolicyinfoes API.
+type StoragePolicyInfo struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Status StoragePolicyInfoStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// StoragePolicyInfoList contains a list of StoragePolicyInfo.
+type StoragePolicyInfoList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []StoragePolicyInfo `json:"items"`
+}
